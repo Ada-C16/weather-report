20fdac6990ebf0cc984d5b385bb6ffb130dda23d
Afina <afinawalton95@gmail.com>
2021-12-06 14:25:55 -0800

    Add ids to elements and create variables to store changing elements

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let city = document.getElementById("cityName");
+const resetButton = document.getElementById("resetButton");
+const increaseTemp = document.getElementById("increaseTemp");
+const decreaseTemp = document.getElementById("decreaseTemp");
+let temp = document.getElementById("temp").firstChild;
+let sky = document.getElementById("sky");
+let garden = document.getElementById("garden");
