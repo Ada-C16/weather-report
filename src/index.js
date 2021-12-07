8a726dc71d5b1dde499782974c2e52007537a658
karishma <karishmacarterjohnson@gmail.com>
2021-12-07 15:06:52 -0800

    make h1 'weather report'

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const mainSection = document.getElementById('main-section');
+
+// h1
+const weatherHeading = document.createElement('h1');
+weatherHeading.textContent = "Weather Report";
+mainSection.appendChild(weatherHeading)
+
+// h2
+const city = document.createElement('h2') ;
+city.setAttribute("contenteditable","true");
+city.textContent = "Mountain House";
+mainSection.appendChild(city);
+
