69a3b15600d93e074eab563f02dd04af92539360
Rhyannon <rhyannonjoy@gmail.com>
2021-12-08 14:53:02 -0700

    Testing jS geolocation functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('wAHT');
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+    });
+  } else {
+    h1.textContent = 'This is not working';
+  }
+});
