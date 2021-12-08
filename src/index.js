efd3fa5dab42427865b12324f814a00650d2d336
Zandra Nguyen <zandra.eng@gmail.com>
2021-12-08 12:52:56 -0800

    need to work on temperature feature

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
-// select the HTML element the even will occur on
+// ***** select the HTML element the even will occur on ****
+// ***** make a function to run when it occurs *****
 
-// make a function to run when it occurs
+const state = {
+  temp: 49,
+};
+// temperature settings
+const updateTempStyles = (temp) => {
+  const tempArrow = document.getElementById('temp-number');
+  let color = 'teal';
+  if (temp >= 80) {
+    color = 'red';
+  } else if (temp >= 70) {
+    color = 'orange';
+  } else if (temp >= 60) {
+    color = 'yellow';
+  } else if (temp >= 50) {
+    color = 'green';
+  }
+  tempArrow.classList = color;
+};
 
-// register that function as an 'event listening'
-const registerEventHandlers = () => {};
+const updateTemp = (temp) => {
+  const tempContainer = document.getElementById('temp-number');
+  tempContainer.textContent = temp;
+  updateTempStyles(state.temp);
+  updateGarden(state.temp);
+};
 
-document.addEventListener('DOMContentLoaded', registerEventHandlers);
+const increaseTemp = () => {
+  state.temp += 1;
+  updateTemp(state.temp);
+};
 
-// date & time
+const decreaseTemp = () => {
+  state.temp -= 1;
+  updateTemp(state.temp);
+};
+
+// changing sky image when select different sky option
+const selectSky = document.getElementById('selectSky').value;
+selectSky.addEventListener('change', event);
+
+// ******** register that function as an 'event listening' ********
+const registerEventHandlers = () => {
+  updateSky();
+  const skySelect = document.getElementById('selectSky');
+  skySelect.addEventListener('change', updateSky);
+};
+
+// set date of the week
 let day;
 switch (new Date().getDay()) {
   case 0:
@@ -32,3 +73,5 @@ switch (new Date().getDay()) {
     day = 'Saturday';
 }
 document.getElementById('date-time').innerHTML = day;
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
