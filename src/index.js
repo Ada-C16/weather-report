aea2e200c49f12d2243199a3a20a7f300c66280d
Laurel S <ls60@protonmail.com>
2021-12-07 17:23:13 -0800

    tempStyle func changes color based on temp range

--- src/index.js
+++ src/index.js
@@ -11,13 +11,45 @@ const displayTemp = () => {
     increaseButton.addEventListener('click', () => {
         temp += 1;
         tempBox.textContent = temp;
+        // console.log('to temp style')
+        tempStyle(temp, tempBox);
     });
     // add event listener to decrease temp button
     const decreaseButton = document.querySelector('#decrease');
     decreaseButton.addEventListener('click', () => {
         temp -= 1;
         tempBox.textContent = temp;
+        // console.log('to temp style')
+        tempStyle(temp, tempBox);
     })
+}
+
+const tempStyle = (temp, tempBox) => {
+    // const tempBox = document.getElementById('temp-box')
+    tempBox.removeAttribute('class');
+    if (temp >=80) {
+        // const tempBox = document.getElementById('temp-box');
+        // tempBox.classList.remove('seventy-degrees', 'hi')
+        // tempBox.removeAttribute('class');
+        tempBox.classList.add('eighty-degrees');
+    } else if (temp >= 70) {
+        // const tempBox = document.getElementById('temp-box');
+        // tempBox.classList.remove('eighty-degrees', 'seventy-degrees', 'sixty-degrees', 'fifty-degrees')
+        // tempBox.removeAttribute('class');
+        tempBox.classList.add('seventy-degrees');
+    } else if (temp >= 60) {
+        // const tempBox = document.getElementById('temp-box');
+        // tempBox.removeAttribute('class');
+        tempBox.classList.add('sixty-degrees');
+    } else if (temp >= 50) {
+        // const tempBox = document.getElementById('temp-box');
+        // tempBox.removeAttribute('class');
+        tempBox.classList.add('fifty-degrees');
+    } else {
+        // const tempBox = document.getElementById('temp-box')
+        // tempBox.removeAttribute('class');
+        tempBox.classList.add('forty-degrees')
+    }
 }
 
 
