51c01548d7ba3eb4e110ceaa1e3f9ed3dc816d7d
Laurel S <ls60@protonmail.com>
2021-12-07 17:30:52 -0800

    Changes landscapes dependent on temp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// set initial temp
 let temp = 55;
 
 // select temp box
@@ -26,29 +27,35 @@ const displayTemp = () => {
 
 const tempStyle = (temp, tempBox) => {
     // const tempBox = document.getElementById('temp-box')
+    const landscape = document.getElementById('landscape')
     tempBox.removeAttribute('class');
     if (temp >=80) {
         // const tempBox = document.getElementById('temp-box');
         // tempBox.classList.remove('seventy-degrees', 'hi')
         // tempBox.removeAttribute('class');
         tempBox.classList.add('eighty-degrees');
+        landscape.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂'
     } else if (temp >= 70) {
         // const tempBox = document.getElementById('temp-box');
         // tempBox.classList.remove('eighty-degrees', 'seventy-degrees', 'sixty-degrees', 'fifty-degrees')
         // tempBox.removeAttribute('class');
         tempBox.classList.add('seventy-degrees');
+        landscape.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷'
     } else if (temp >= 60) {
         // const tempBox = document.getElementById('temp-box');
         // tempBox.removeAttribute('class');
         tempBox.classList.add('sixty-degrees');
+        landscape.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃'
     } else if (temp >= 50) {
         // const tempBox = document.getElementById('temp-box');
         // tempBox.removeAttribute('class');
         tempBox.classList.add('fifty-degrees');
+        landscape.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲'
     } else {
         // const tempBox = document.getElementById('temp-box')
         // tempBox.removeAttribute('class');
         tempBox.classList.add('forty-degrees')
+        landscape.textContent = '🌬🌨 ️⛄🌬🌨 ️⛄🌬🌨 ️⛄🌬🌨 ️⛄'
     }
 }
 
