53722a4418bcda0f0ecbf2f40779029aec152336
exitexit <eunicegore@gmail.com>
2021-12-08 13:19:33 -0800

    wireframe first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const resetCity = () => {
+  let inputCityVariable = document.getElementById('input-city').value;
+  document.getElementById('seattleCity').innerHTML = inputCityVariable;
+};
+// const changeTemp = () => {
+//   let inputTempVariable = document.getElementById('input-temp').value;
+//   document.getElementById('current-temp').innerHTML = inputTempVariable;
+// };
