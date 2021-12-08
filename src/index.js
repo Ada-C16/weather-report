8718495efcc1214547c8c1efb01a7fc449c8d7c0
Zandra Nguyen <zandra.eng@gmail.com>
2021-12-07 22:34:00 -0800

    add day of the week to temperature container

--- src/index.js
+++ src/index.js
@@ -3,7 +3,32 @@
 // make a function to run when it occurs
 
 // register that function as an 'event listening'
-
 const registerEventHandlers = () => {};
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+// date & time
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = 'Sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 2:
+    day = 'Tuesday';
+    break;
+  case 3:
+    day = 'Wednesday';
+    break;
+  case 4:
+    day = 'Thursday';
+    break;
+  case 5:
+    day = 'Friday';
+    break;
+  case 6:
+    day = 'Saturday';
+}
+document.getElementById('date-time').innerHTML = day;
