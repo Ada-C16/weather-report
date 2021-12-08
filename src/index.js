c1e4ea87041faa4fd8463b62e46db49eabea571d
Zandra Nguyen <zandra.eng@gmail.com>
2021-12-07 20:03:42 -0800

    add background and title

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// select the HTML element the even will occur on
+
+// make a function to run when it occurs
+
+// register that function as an 'event listening'
+
+const registerEventHandlers = () => {};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
