4f9001405c7e96c935557795104ae5412805cba1
karishma <karishmacarterjohnson@gmail.com>
2021-12-07 18:04:04 -0800

    refactor sky mode for color reference as class from id

--- src/index.js
+++ src/index.js
@@ -5,29 +5,34 @@ const weatherHeading = document.createElement('h1');
 weatherHeading.textContent = "Weather Report";
 mainSection.appendChild(weatherHeading)
 
-// h2
+// LOCATION
 const city = document.createElement('h2') ;
 city.setAttribute("contenteditable","true");
 city.textContent = "Mountain House";
 mainSection.appendChild(city);
 
-// make sky changer zone
+// SKY MODE
 const skyMode = ["Morning", "Midday", "Sunset", "Midnight"];
 const skyContainer = document.createElement('div');
 skyContainer.setAttribute("id", "sky-container");
 
 skyMode.forEach(mode => {
     let skyTheme = document.createElement('div');
-    skyTheme.setAttribute("class", "sky-color");
-    skyTheme.setAttribute("id", `${mode}`);
-    // skyTheme.setAttribute("onclick", //function call)
+    skyTheme.setAttribute("class", `sky-color ${mode}`);
+    skyTheme.setAttribute("onclick", "changeSky");
     skyContainer.appendChild(skyTheme);
 });
 
 mainSection.append(skyContainer);
 
+function changeSky () {
+    let element = document.querySelector("sky-color");
+    // use id to choose toggle and apply to body
+    document.body.classList.toggle(`${element.class[0]}`)
+}
 
-// add temp gauge
+
+// WEATHER STATION
 
 const state = {
     temp: 92
