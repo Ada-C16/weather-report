b22e3a4804d81e8a19b85fec9436a2c8c34d01de
jacy <jacyyang04@gmail.com>
2021-12-08 15:04:55 -0800

    add buttons, new containers, and format temp display

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,38 @@
 const tempElement = document.getElementById('temp');
 
 // TEMP DISPLAY
+const emojiHolder = document.querySelector('#emoji-holder');
+console.log(emojiHolder);
 
-const emojiHolder = document.getElementById('emoji-holder');
-
-const tempUp = document.createElement('p');
+const tempUp = document.createElement('button');
 tempUp.textContent = '⬆️';
-tempUp.className = 'temp-changes';
 emojiHolder.appendChild(tempUp);
 
-const tempDown = document.createElement('p');
+const tempDown = document.createElement('button');
 tempDown.textContent = '⬇️';
-tempDown.className = 'temp-changes';
 emojiHolder.appendChild(tempDown);
 tempElement.appendChild(emojiHolder);
 
-const tempNum = document.createElement('p');
+const tempNum = document.createElement('h3');
 tempNum.textContent = 68;
-tempNum.className = 'temp-text';
+tempNum.id = 'temp-text';
 tempElement.appendChild(tempNum);
+
+// ADD TEMP FUNCTIONALITY
+let webTemp = 68;
+
+let currentTemp = (document.getElementById('temp-text').innerText = webTemp);
+console.log(currentTemp);
+
+const upTempFunc = () => {
+  webTemp = webTemp + 1;
+  document.getElementById('temp-text').innerText = webTemp;
+};
+
+const downTempFunc = () => {
+  webTemp = webTemp + 1;
+  document.getElementById('temp-text').innerText = webTemp;
+};
 
 // SKY DISPLAY
 
@@ -33,3 +47,9 @@ const weatherGarden = document.getElementsByClassName('garden-display');
 
 // Need to add this for eventListener
 // document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+// if (document.readyState !== 'loading') {
+//   loadTasks();
+// } else {
+//   document.addEventListener('DOMContentLoaded', loadTasks);
+// }
