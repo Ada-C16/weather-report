a8e600ff72724d97568dad86edc9a0a90ca62423
bailey <baileyholderman@gmail.com>
2021-12-08 14:39:06 -0800

    adds initial HTML content and comments in JS file to guide organization

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// WAVE 2
+// an element that displays the temperature
+// one clickable element to increase temperature
+// one clickable element to decrease temperature
+// An element that displays a landscape
+
+// OPTIONAL ENHANCEMENTS-- button to switch between celsius and fahrenheit
+
+
+
+// WAVE 3
+// dropdown menu to set the sky type
+// an element that displays the sky
+
+
+
+// WAVE 4 
+// an element that displays the city name
+// an element that lets you change city name
+
+
+
+// WAVE 5
+// reset button to put city name back to default
