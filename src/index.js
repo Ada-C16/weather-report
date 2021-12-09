8f4b151ccbac2e92ff28a7bc14be0b4fcd7b08ef
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2021-12-08 16:00:20 -0800

    Used grid to add 4 containers. Added buttons, drop down menu, text entry bar. Started writing JS for functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hi")
+const state = {
+    tempCount = 0
+}
+
+const decreaseTemp = () => {
+    state.tempCount -= 1;
+    const tempCountContainer = document.querySelector("#tempCount")
+    tempCountContainer.textContent = `${state.tempCount}`
+}
+
+const registerEventHandlers = () => {
+    const tempButton = document.querySelector("#addToCounter")
+    tempButton.addEventListener("click", addTemp)
+}
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
