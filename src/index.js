41bcbae4e0c766d4b83a8bae939ad8b35410a850
Tirhas Gebreyohannes <tirhastse@gmail.com>
2021-12-09 14:23:47 -0800

    adds Event listener for reset button

--- src/index.js
+++ src/index.js
@@ -2,44 +2,80 @@
 
 const state = {
     temperature:0
+}
+
+const getSelectedCity = () =>{
+        const showCityTyped = document.querySelector("#city").value;
+        const cityName =document.querySelector("#lovelyCity");
+        cityName.textContent = event.target.value
+}
+
+const addReset = () =>{
+    let input = document.querySelector("#city");
+    input.value = ""
 
 }
+
 const temperatureReadingElement = document.querySelector("#temperatureReading");
 const changeColor = () => {
-    if (state.temperature >= 8){
+    if (state.temperature >= 80){
         temperatureReadingElement.className="burning";
-    }else if(state.temperature >=7){
-        temperatureReadingElement.className="worning"
-    }else if(state.temperature >= 6){
-        temperatureReadingElement.className="sunny"
-    }else if (state.temperature >=5){
-        temperatureReadingElement.className="cold"
+    }else if(state.temperature >=70){
+        temperatureReadingElement.className="worning";
+    }else if(state.temperature >= 60){
+        temperatureReadingElement.className="sunny";
+    }else if (state.temperature >=50){
+        temperatureReadingElement.className="cold";
     }else{
-        temperatureReadingElement.className="freezing"
+        temperatureReadingElement.className="freezing";
     }
 
     }
 
-    const changeImage = () =>{
-        const displayImageElement = document.querySelector("#displayFeature");
-        const newImage=document.createElement("span")
-        if (state.temperature >= 8){
-            newImage.innerHTML="🌵__🐍_🦂_🌵🌵__🐍_🏜_";
-        }else if(state.temperature >= 7){
-            newImage.innerHTML="🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
-        }else if(state.temperature >= 6){
-            newImage.innerHTML="🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
-        }else if(state.temperature <= 5){
-            newImage.innerHTML="🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
-        }
-        if (displayImageElement.childElementCount >= 1){
-            displayImageElement.replaceChildren(newImage);
-        }else{
-            displayImageElement.appendChild(newImage);
-            
-        }
+const changeImage = () =>{
+    const displayImageElement = document.querySelector("#displayFeature");
+    const newImage=document.createElement("span")
+    if (state.temperature >= 80){
+        newImage.innerHTML="🌵__🐍_🦂_🌵🌵__🐍_🏜_";
+    }else if(state.temperature >= 70){
+        newImage.innerHTML="🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+    }else if(state.temperature >= 60){
+        newImage.innerHTML="🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+    }else if(state.temperature <= 59){
+        newImage.innerHTML="🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    }
+    if (displayImageElement.childElementCount >= 1){
+        displayImageElement.replaceChildren(newImage);
+    }else{
+        displayImageElement.appendChild(newImage);
+        
+    }
+
+    }
+
+    const getSelectedWeather = () =>{
+        const showSelectedWeather = document.querySelector("#sky").value;
+        console.log(showSelectedWeather)
+        const displayWeather = document.querySelector("#displayWeather");
+        const newWeather = document.createElement("span");
+        if(showSelectedWeather==="sunny"){
+            newWeather.innerHTML ="☁️ ☁️ ☁️ ☀️ ☁️ ☁️"
+        }else if(showSelectedWeather === "cloudy"){
+            newWeather.innerHTML="☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️"  
+        }else if(showSelectedWeather === "rainy" ){
+            newWeather.innerHTML="🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧"
     
+        }else if(showSelectedWeather==="snowy"){
+            newWeather.innerHTML="🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨";
         }
+        if (displayWeather.children.length >=1){
+            displayWeather.replaceChildren(newWeather);
+            console.log("first")
+        }else{
+            displayWeather.appendChild(newWeather);
+            console.log("second")
+        } 
+    }
 
 
 const increaseTemperatureReading = () =>{
@@ -58,8 +94,13 @@ const reduceTemperatureReading = () =>{
 const registerEventHandlers = (event) => {
     const addTemperatureButton =document.getElementById("addTemperatureButton");
     const reduceTemperatureButton =document.getElementById("reduceTemperatureButton");
-    console.log(addTemperatureButton);
+    const citySelected=document.querySelector("#city");
+    const addResetButton = document.getElementById("addResetButton")
+    const addWeatherSelection = document.getElementById("sky")
     addTemperatureButton.addEventListener("click",increaseTemperatureReading);
     reduceTemperatureButton.addEventListener("click",reduceTemperatureReading);
+    citySelected.addEventListener("input",getSelectedCity)
+    addResetButton.addEventListener("click",addReset)
+    addWeatherSelection.addEventListener("change",getSelectedWeather)
 };
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
