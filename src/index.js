10a4256b4c4188ba8d53eacba589728ba0d57776
bailey <baileyholderman@gmail.com>
2021-12-08 16:18:57 -0800

    passes Wave 2

--- src/index.js
+++ src/index.js
@@ -6,19 +6,65 @@
 
 // OPTIONAL ENHANCEMENTS-- button to switch between celsius and fahrenheit
 
+// separate function to set text color and landscape
+// Make check for class name equal to celsius or fahranheit
 
+// Grabbing my html elements
+const state = {
+  temp: 70,
+};
+
+const increaseTemp = document.getElementById('increase-temp');
+const decreaseTemp = document.getElementById('decrease-temp');
+const convertTemp = document.getElementById('convert-temp');
+const garden = document.getElementById('garden');
+
+const updateByTemp = (newTemp) => {
+  if (newTemp >= 80) {
+    temp.style.color = '#EA0009';
+    garden.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+  } else if (70 <= newTemp) {
+    temp.style.color = '#F0940A';
+    garden.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+  } else if (60 <= newTemp) {
+    temp.style.color = '#F4D10A';
+    garden.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+  } else if (50 <= newTemp) {
+    temp.style.color = '#1F7001';
+    garden.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  } else if (newTemp <= 49) {
+    temp.style.color = '#256D6C';
+    garden.textContent = '🌲🌲⛄️🌲⛄️🌲☃❄🌲🌲⛄️❄️🌲';
+  }
+};
+
+const updateTemp = () => {
+  const tempElement = document.getElementById('temp');
+  tempElement.textContent = `${state.temp}`;
+};
+
+const incrementTemp = () => {
+  state.temp += 1;
+  updateTemp();
+  updateByTemp(state.temp);
+};
+
+const decrementTemp = () => {
+  state.temp -= 1;
+  updateTemp();
+  updateByTemp(state.temp);
+};
+
+increaseTemp.addEventListener('click', incrementTemp);
+decreaseTemp.addEventListener('click', decrementTemp);
 
 // WAVE 3
 // dropdown menu to set the sky type
 // an element that displays the sky
 
-
-
-// WAVE 4 
+// WAVE 4
 // an element that displays the city name
 // an element that lets you change city name
-
-
 
 // WAVE 5
 // reset button to put city name back to default
