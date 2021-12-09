a3559bfb65c0c85d170e5c03cc1db3d7ab48380a
Areeba Rashid <areeba.r.qureshi@gmail.com>
2021-12-08 18:19:47 -0800

    working buttons and input

--- src/index.js
+++ src/index.js
@@ -1,94 +1,166 @@
 //Increase/Decrease
 
-// var count = 0;
+// let count = 0;
 // document.getElementById("degrees").textContent = count;
-// var down = document.getElementById("down-button");
-// var up = document.getElementById("up-button");
+// let degreeNum = document.querySelector("#degrees");
 
+// let upButton = document.getElementById("up-button");
+// let downButton = document.getElementById("down-button");
 
-// function decreaseTemp(){
-//     count = count - 1;
-//     document.getElementById("degrees").textContent = count;
-// }
+//let's try to use state
 
-// function increaseTemp(){
+// upButton.addEventListener('click', () => {
 //     count = count + 1;
+//     color(count);
 //     document.getElementById("degrees").textContent = count;
-// }
+// });
 
-// have to include onclick="increaseTemp()" in place of id in html tag for the above to work
+// downButton.addEventListener('click', () => {
+//     count = count - 1;
+//     color(count);
+//     document.getElementById("degrees").textContent = count;
+// });
 
-let count = 0;
-document.getElementById("degrees").textContent = count;
-let degreeNum = document.querySelector("#degrees");
-
-let upButton = document.getElementById("up-button");
-let downButton = document.getElementById("down-button");
-
-
-
-
-upButton.addEventListener('click', () => {
-    count = count + 1;
-
-    if(count <= 49){
-        degreeNum.className = "freezing";
-    }
-    else if(count <= 59){
-        degreeNum.className = "cold";
-    }
-    else if(count <= 69){
-        degreeNum.className = "warm";
-    }
-    else if(count <= 79){
-        degreeNum.className = "hot";
-    }
-    else{
-        degreeNum.className = "scorching";
-    }
-
-    document.getElementById("degrees").textContent = count;
-});
-
-// upButton.addEventListener('change', () => {
-//     let degreeNum = document.querySelector("#degrees");
-
+// function color(count){
 //     if(count <= 49){
 //         degreeNum.className = "freezing";
 //     }
 //     else if(count <= 59){
 //         degreeNum.className = "cold";
 //     }
-// });
-// downButton.addEventListener('change', () => {
-//     let degreeNum = document.querySelector("#degrees");
-
-//     if(count <= 49){
-//         degreeNum.className = "freezing";
+//     else if(count <= 69){
+//         degreeNum.className = "warm";
 //     }
-// });
+//     else if(count <= 79){
+//         degreeNum.className = "hot";
+//     }
+//     else{
+//         degreeNum.className = "scorching";
+//     }
+// }
 
-downButton.addEventListener('click', () => {
-    count = count - 1;
 
-    if(count <= 49){
+//make and change landscapes
+
+// function landscape(count){
+//     if count <= 32{
+//         document.getElementById("surprise").innerHTML="<img src='images/aftersurprise.png' />";
+//     }
+//     else if count <= 32{
+//         document.getElementById("surprise").innerHTML="<img src='images/aftersurprise.png' />";
+//     }
+//     else if count <= 32{
+//         document.getElementById("surprise").innerHTML="<img src='images/aftersurprise.png' />";
+//     }
+//     else if count <= 32{
+//         document.getElementById("surprise").innerHTML="<img src='images/aftersurprise.png' />";
+//     }
+// };
+
+
+
+//state
+const state = {
+    temp: 0,
+};
+
+//buttons
+const increase = () => {
+    state.temp += 1;
+    color();
+    landscape();
+    document.getElementById("degrees").textContent = `${state.temp} F`;
+};
+const decrease = () => {
+    state.temp -= 1;
+    color();
+    landscape();
+    document.getElementById("degrees").textContent = `${state.temp} F`;
+};
+
+//dropdown
+const sky = (event) => {
+    scene = document.querySelector("#skyscape");
+
+    if (event.target.value === 'sunny') {
+		scene.className = "sunny";
+        scene.textContent = "☁️ ☁️ ☁️ ☀️ ☁️ ☁️";
+	}
+    else if(event.target.value === 'cloudy'){
+        scene.className = "cloudy";
+        scene.textContent = "☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️";
+    }
+    else if(event.target.value === 'rainy'){
+        scene.className = "rainy";
+        scene.textContent = "🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧";
+    }
+    else{
+        scene.className = "snowy";
+        scene.textContent = "🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨";
+    }
+
+};
+
+//Event handler 
+
+const registerEventHandlers = (event) => {
+    const upButton = document.getElementById("up-button");
+    const downButton = document.getElementById("down-button");
+    const dropDown = document.getElementById("dropdown");
+
+    upButton.addEventListener('click', increase);
+    downButton.addEventListener('click', decrease);
+    dropDown.addEventListener('change', sky);
+}
+
+//helper functions
+//-------- button related functions ---------//
+function color(){
+    degreeNum = document.querySelector("#degrees");
+    if(state.temp <= 32){
         degreeNum.className = "freezing";
     }
-    else if(count <= 59){
+    else if(state.temp <= 49){
+        degreeNum.className = "really-cold";
+    }
+    else if(state.temp <= 59){
         degreeNum.className = "cold";
     }
-    else if(count <= 69){
+    else if(state.temp <= 69){
         degreeNum.className = "warm";
     }
-    else if(count <= 79){
+    else if(state.temp <= 79){
         degreeNum.className = "hot";
     }
     else{
         degreeNum.className = "scorching";
     }
+}
+function landscape(){
+    scene = document.querySelector("#landscape");
+    if(state.temp <= 0){
+        scene.textContent = "☠️❄️☠️❄️☠️❄️☠️❄️☠️❄️☠️❄️☠️❄️☠️❄️☠️❄️☠️❄️☠️";
+    }
+    else if(state.temp <= 32){
+        scene.textContent = "⛄️❄️🌲❄️⛄️❄️🌲❄️⛄️❄️🌲❄️⛄️❄️🌲";
+    }
+    else if(state.temp <= 45){
+        scene.textContent = "🍁☕🍂🍁☕🍂🍁☕🍂🍁☕🍁🍂☕🍁🍂☕";
+    }
+    else if(state.temp <= 55){
+        scene.textContent = "🌸🌿🌼_🌧️🌷🌻🌿_☘️🌱_🌻🌷🌧️";
+    }
+    else if(state.temp <= 70){
+        scene.textContent = "🍉☀️🍎☀️🍐☀️🍉☀️🍎☀️🍐☀️🍉☀️🍎☀️🍐";
+    }
+    else if(state.temp <= 85){
+        scene.textContent = "🏖️🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊";
+    }
+    else{
+        scene.textContent = "🍳🍳🍳🍳🍳🍳🍳🍳🍳🍳🍳🍳🍳";
+    }
+}
 
-    document.getElementById("degrees").textContent = count;
-});
+function skyscape(){}
 
-//change text color 
-
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
