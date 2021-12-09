da4cdc014075a13e57cc64488d1d44bc6cfc53e1
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-12-09 14:03:07 -0800

    final touches, all JS works

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-const tempChange = () => {
+const changeTemp = () => {
   const temperature = document.getElementById('tempNum');
   const tempUpButton = document.getElementById('up');
   const tempDownButton = document.getElementById('down');
 
   tempUpButton.addEventListener('click', () => {
     temperature.innerHTML = parseInt(temperature.innerHTML) + 1;
+    changeLandscape(temperature.innerHTML, tempType.innerHTML);
   });
   tempDownButton.addEventListener('click', () => {
     temperature.innerHTML = parseInt(temperature.innerHTML) - 1;
+    changeLandscape(temperature.innerHTML, tempType.innerHTML);
   });
 };
 
@@ -36,30 +38,104 @@ const convertTemp = () => {
   });
 };
 
-const landscape = () => {
+const initialLandscape = () => {
+  const temperature = document.getElementById('tempNum');
+  const tempType = document.getElementById('tempType');
+  changeLandscape(temperature.innerHTML, tempType.innerHTML);
+};
+
+const changeLandscape = (temperature, tempType) => {
   const sky = document.getElementById('sky');
   const plants = document.getElementById('plants');
   const ground = document.getElementById('ground');
-
-  const temperature = document.getElementById('tempNum');
-
+  ground.innerText = grass;
   sky.style.whiteSpace = 'pre';
   plants.style.whiteSpace = 'pre';
   ground.style.whiteSpace = 'pre';
 
-  sky.innerText = snowSky;
-  plants.innerText = snowPlants;
-  ground.innerText = grass;
+  const snowLandscape = [snowSky, snowPlants];
+  const rainLandscape = [rainySky, rainyPlants];
+  const springLandscape = [springSky, springPlants];
+  const autumnLandscape = [springSky, autumnPlants];
+
+  if (tempType === '℉') {
+    console.log('fahrenheit');
+    if (temperature < 32) {
+      sky.innerText = snowLandscape[0];
+      plants.innerText = snowLandscape[1];
+    } else if (temperature < 50) {
+      sky.innerText = autumnLandscape[0];
+      plants.innerText = autumnLandscape[1];
+    } else if (temperature < 70) {
+      sky.innerText = rainLandscape[0];
+      plants.innerText = rainLandscape[1];
+    } else {
+      sky.innerText = springLandscape[0];
+      plants.innerText = springLandscape[1];
+    }
+  } else {
+    if (temperature < 10) {
+      sky.innerText = snowLandscape[0];
+      plants.innerText = snowLandscape[1];
+    } else if (temperature < 20) {
+      sky.innerText = autumnLandscape[0];
+      plants.innerText = autumnLandscape[1];
+    } else if (temperature < 30) {
+      sky.innerText = rainLandscape[0];
+      plants.innerText = rainLandscape[1];
+    } else {
+      sky.innerText = springLandscape[0];
+      plants.innerText = springLandscape[1];
+    }
+  }
+};
+
+const changeSky = () => {
+  const skyButton = document.getElementById('skyButton');
+  const skyForm = document.getElementById('skyForm');
+  const garden = document.getElementById('garden');
+  const skies = {
+    sunrise: '#000000',
+    bluesky: '#00A1FF',
+    clouds: '#D3D3D3',
+    overcast: '#E4E3FC',
+    starrynight: '#0B0677',
+    rainbow: '#FF00FF',
+    kittens: '#FFB6C1',
+  };
+
+  skyButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    value = skyForm.options[skyForm.selectedIndex].value;
+    console.log(value);
+    garden.style.backgroundColor = skies[value];
+  });
+};
+
+const changeCity = () => {
+  const cityButton = document.getElementById('cityButton');
+  const city = document.querySelector('#city');
+
+  const headerCity = document.getElementById('header-city');
+
+  cityButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    headerCity.innerHTML = city.value;
+  });
 };
 
 if (document.readyState !== 'loading') {
-  tempChange();
+  changeTemp;
   convertTemp();
-  landscape();
+  initialLandscape();
+  changeSky();
+  changeCity();
 } else {
   document.addEventListener('DOMContentLoaded', () => {
+    changeTemp();
     convertTemp();
-    tempChange();
-    landscape();
+    initialLandscape();
+    changeSky();
+    changeCity();
   });
 }
