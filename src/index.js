e3f67d9a83ed8558833a33cc2f82046f2cc50fb2
starseed2021 <tiff.harper18@gmail.com>
2021-12-09 17:41:51 -0500

    implemented code to change colors by various temperatures

--- src/index.js
+++ src/index.js
@@ -17,9 +17,74 @@ const changePhoto = () => {
    }
 };
 
-// Temperature Counter
-// Function to change colors according to temperature
 
+
+// Temporary Temperature Counter
+let tempValue = 0;
+
+const updateTemp = tempValue => {
+    const tempCount = document.getElementById("tempCount");
+    tempCount.textContent = `Temperature: ${tempValue}`;
+    //update temp colors
+    //update landscape
+};
+
+// Temperature Increase Event Handler
+const tempIncrease = () => {
+    tempValue += 1;
+    updateTemp(tempValue);
+};
+
+// Temperature Decrease Event Handler
+const tempDecrease = () => {
+    tempValue -= 1;
+    updateTemp(tempValue);
+};
+
+
+// Updating Temperature Colors
+const updateTempColor = (currentTemp) => {
+    const tempContainer = document.getElementById("tempCount");
+    let color = "salmon";
+    if (currentTemp >= 80) {
+        color = "red";
+    } else if (currentTemp >= 70) {
+        color = "orange";
+    } else if (currentTemp >= 60) {
+        color = "yellow";
+    } else if (currentTemp >= 50) {
+        color = "green";
+    }
+    tempContainer.classList = color;
+};
+
+// Landscape Event Handler
+const updateLandscape = (currentTemp) => {
+    const landscapeContainer = document.getElementById("landscape");
+    let color = "lightgreen";
+    if (currentTemp >= 80) {
+        color = "lightblue";
+    } else if (currentTemp >= 70) {
+        color = "cadetblue";
+    } else if (currentTemp >= 60) {
+        color = "slategrey";
+    } else if (currentTemp >= 50) {
+        color = "lightgrey";
+    }
+    landscapeContainer.classList = color;
+}
+
+
+// CityName Event Handler
+const inputCityName = () => {
+
+};
+
+
+// CityName Reset Button Handler
+const addResetBtn = () => {
+
+};
 
 
 
@@ -30,11 +95,17 @@ const registerEvents = () => {
     const increaseButton = document.getElementById("clickButtonIn");
     increaseButton.addEventListener("click", () => {
         console.log("clicked");
+        tempIncrease(); 
+        updateTempColor(tempValue);
+        updateLandscape(tempValue);
     });
 
     const decreaseButton = document.getElementById("clickButtonDe");
     decreaseButton.addEventListener("click", () => {
         console.log("clicked2");
+        tempDecrease();
+        updateTempColor(tempValue);
+        updateLandscape(tempValue);
     });
 
     const skySelect = document.getElementById("skySelect");
@@ -49,6 +120,8 @@ const registerEvents = () => {
         console.log("clicked select");
     });
     
+    const cityNameSelect = document.getElementById("cityNameSelect");
+    cityNameSelect.addEventListener("input", ____);
 };
 
 document.addEventListener("DOMContentLoaded", registerEvents);
