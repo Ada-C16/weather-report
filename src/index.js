27359df8e8a9f5ec4617233f1605110dddfe5431
Tirhas Gebreyohannes <tirhastse@gmail.com>
2021-12-08 21:05:14 -0800

    adds landscape according to temperatur

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const state = {
 }
 const temperatureReadingElement = document.querySelector("#temperatureReading");
 const changeColor = () => {
-    if (state.temperature >= 80){
+    if (state.temperature >= 8){
         temperatureReadingElement.className="burning";
-    }else if(state.temperature >=70){
+    }else if(state.temperature >=7){
         temperatureReadingElement.className="worning"
-    }else if(state.temperature >= 60){
+    }else if(state.temperature >= 6){
         temperatureReadingElement.className="sunny"
-    }else if (state.temperature >=50){
+    }else if (state.temperature >=5){
         temperatureReadingElement.className="cold"
     }else{
         temperatureReadingElement.className="freezing"
@@ -20,15 +20,39 @@ const changeColor = () => {
 
     }
 
+    const changeImage = () =>{
+        const displayImageElement = document.querySelector("#displayFeature");
+        const newImage=document.createElement("span")
+        if (state.temperature >= 8){
+            newImage.innerHTML="🌵__🐍_🦂_🌵🌵__🐍_🏜_";
+        }else if(state.temperature >= 7){
+            newImage.innerHTML="🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+        }else if(state.temperature >= 6){
+            newImage.innerHTML="🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+        }else if(state.temperature <= 5){
+            newImage.innerHTML="🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+        }
+        if (displayImageElement.childElementCount >= 1){
+            displayImageElement.replaceChildren(newImage);
+        }else{
+            displayImageElement.appendChild(newImage);
+            
+        }
+    
+        }
+
 
 const increaseTemperatureReading = () =>{
     state.temperature += 1;
     changeColor();
+    changeImage()
     temperatureReadingElement.textContent = `Temprature: ${state.temperature}`;
 };
 
 const reduceTemperatureReading = () =>{
     state.temperature -= 1;
+    changeColor();
+    changeImage();
     temperatureReadingElement.textContent = `Temprature: ${state.temperature}`;
 };
 const registerEventHandlers = (event) => {
