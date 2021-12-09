7601c94133e3a65e5bb15bd736e08565f5af6958
jacy <jacyyang04@gmail.com>
2021-12-08 21:04:31 -0800

    organize eventListener calls

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
-// SELECT ELEMENT TO MODIFY
+const state = {
+  temp: 68,
+};
+
 const tempElement = document.getElementById('temp');
 
-// TEMP DISPLAY
 const tempEmojiHolder = document.querySelector('#temp-emoji-holder');
-// console.log(tempEmojiHolder);
 
 const tempUp = document.createElement('button');
 tempUp.id = 'temp-up';
@@ -16,10 +17,6 @@ tempDown.textContent = '⬇️';
 tempEmojiHolder.appendChild(tempDown);
 tempElement.appendChild(tempEmojiHolder);
 
-const state = {
-  temp: 68,
-};
-
 const tempNum = document.createElement('p');
 tempNum.textContent = state.temp;
 tempNum.id = 'temp-text';
@@ -27,8 +24,7 @@ tempElement.appendChild(tempNum);
 
 // ADD TEMP FUNCTIONALITY
 
-// ADD TEMP
-
+// ADD
 const upTempFunc = () => {
   state.temp = state.temp + 1;
   document.getElementById('temp-text').innerHTML = state.temp;
@@ -36,38 +32,31 @@ const upTempFunc = () => {
   displayFlowers(state.temp);
 };
 
-// SUBTRACT TEMP
+// SUBTRACT
 const downTempFunc = () => {
   state.temp = state.temp - 1;
   document.getElementById('temp-text').innerHTML = state.temp;
 };
 
-const addTempButton = document.getElementById('temp-up');
-const downTempButton = document.getElementById('temp-down');
-
 // REGISTER EVENT HANDLER
-addTempButton.addEventListener('click', () => upTempFunc());
-downTempButton.addEventListener('click', () => downTempFunc());
 
 // WEATHER GARDEN
-const weatherGarden = document.getElementById('garden-display');
 
 const changeFlowerGarden = document.getElementById('garden-flowers');
 
 const changeFlowers = () => {
+  console.log(state.temp);
   if (state.temp < 59) {
     changeFlowerGarden.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
-  } else if (60 < state.temp || state.temp <= 69) {
+  } else if (state.temp <= 69) {
     changeFlowerGarden.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
-  } else if (70 <= state.temp || state.temp <= 79) {
+  } else if (state.temp <= 79) {
     changeFlowerGarden.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
   } else {
     changeFlowerGarden.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
   }
 };
-changeFlowerGarden.addEventListener();
-
-// WAVE 3&4
+changeFlowerGarden.addEventListener('ADD EVENT NAME', () => changeFlowers());
 
 // SKY DISPLAY
 
@@ -75,17 +64,17 @@ const skyElement = document.getElementById('sky');
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function changeSky() {
+const changeSky = () => {
   document.getElementById('myDropdown').classList.toggle('show');
-}
+};
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName('dropdown-content');
-    var i;
+    let dropdowns = document.getElementsByClassName('dropdown-content');
+    let i;
     for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+      let openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
@@ -97,22 +86,13 @@ window.onclick = function (event) {
 const cityElement = document.getElementById('city');
 
 // Need to add this for eventListener
-// document.addEventListener('DOMContentLoaded', registerEventHandlers);
 
-// const registerEventHandlers = (event) => {
-//   console.log('in registerEventHandlers:', event);
-//   // ... other registerEventHandler functionality
-// };
+const registerEventHandlers = (event) => {
+  const addTempButton = document.getElementById('temp-up');
+  addTempButton.addEventListener('click', () => upTempFunc());
 
-// const registerEventHandlers = (event) => {
-//   const addTempButton = document.getElementById('temp-up');
-//   addTempButton.addEventListener('click', addCrab);
-// };
+  const downTempButton = document.getElementById('temp-down');
+  downTempButton.addEventListener('click', () => downTempFunc());
+};
 
-// document.addEventListener('DOMContentLoaded', registerEventHandlers);
-
-// if (document.readyState !== 'loading') {
-//   __func__;
-// } else {
-//   document.addEventListener('DOMContentLoaded', _____);
-// }
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
