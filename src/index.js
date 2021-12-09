b72b71ab0cd808a8eb20d632eabad8b35d602e21
Ana Gabriele <gabbanasoares@gmail.com>
2021-12-08 16:13:02 -0800

    Create function to handle Temperature

--- src/index.js
+++ src/index.js
@@ -27,9 +27,39 @@ const resetCityName = function () {
 const temperatureValue = document.getElementById('temperature-value');
 const decrementButton = document.getElementById('decrement');
 const increaseButton = document.getElementById('increase');
+// const displayLandscape = document.getElementById('display-landscape');
+
 const stateTemp = { value: 0 };
+const landscape = {
+  winter: '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲',
+  freezing: '❄️❄️❄️⛄️❄️❄️⛄️❄️❄️⛄️❄️❄️❄️',
+  autumn: '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃🍃',
+  spring: '🌸🌿🌼🌿🌷🌻🌿_☘️🌱_🌻🌷🌷',
+  summer: '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂🦂',
+};
+
+const changeColorAndLand = (value) => {
+  const displayLandscape = document.getElementById('display-landscape');
+  if (stateTemp.value >= 80) {
+    temperatureValue.classList = 'red-text';
+    displayLandscape.textContent = landscape.summer;
+  } else if (stateTemp.value >= 70 && stateTemp.value <= 79) {
+    temperatureValue.classList = 'orange-text';
+    displayLandscape.textContent = landscape.spring;
+  } else if (stateTemp.value >= 60 && stateTemp.value <= 69) {
+    temperatureValue.classList = 'yellow-text';
+    displayLandscape.textContent = landscape.autumn;
+  } else if (stateTemp.value >= 35 && stateTemp.value <= 59) {
+    temperatureValue.classList = 'green-text';
+    displayLandscape.textContent = landscape.winter;
+  } else {
+    temperatureValue.classList = 'white-text';
+    displayLandscape.textContent = landscape.freezing;
+  }
+};
 
 const changeTemperature = function (event) {
+  changeColorAndLand(stateTemp.value);
   if (event.target.id === 'increment') {
     stateTemp.value++;
     temperatureValue.innerHTML = stateTemp.value;
@@ -39,7 +69,16 @@ const changeTemperature = function (event) {
   }
 };
 
-// Events
+//sky
+
+const sky = document.getElementsById('sky-type');
+
+for (item in sky) {
+  console.log(item);
+}
+
+// REGISTER EVENTS
+
 const registerEventsListeners = function () {
   resetButton.addEventListener('click', resetCityName);
   cityInput.addEventListener('input', updateCityValue);
