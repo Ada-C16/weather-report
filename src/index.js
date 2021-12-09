5bd8cae2fcc54fba8ba8b9c030af0fb8bae94baa
Katie <diazkatie@gmail.com>
2021-12-08 18:42:07 -0800

    colors and landscape change with temp

--- src/index.js
+++ src/index.js
@@ -1,21 +1,77 @@
-// select p by id, adjust temp variable, set p to temp variable
-// Create a state object that will hold the temp number
+// Temperature //
 const state = {
   tempByDegree: 75,
 };
 
-const pushButton = () => {
+const clickButtonUp = () => {
   state.tempByDegree += 1;
-  const tempChanger = document.querySelector('#temp');
-  temp.textContent = `${state.tempByDegree}`;
+  const tempElement = document.querySelector('#temp');
+  tempElement.textContent = `${state.tempByDegree}`;
+  color = changeTempColor();
+  tempElement.className = color;
+  //landscape
+  const landElement = document.querySelector('#landscape');
+  landscape = lanscapeScale();
+  landElement.textContent = landscape;
 };
 const tempUp = document.querySelector('#tempUp');
-tempUp.addEventListener('click', pushButton);
+tempUp.addEventListener('click', clickButtonUp);
 
-const buttonDown = () => {
+const clickButtonDown = () => {
   state.tempByDegree -= 1;
   const tempChanger = document.querySelector('#temp');
-  temp.textContent = `${state.tempByDegree}`;
+  tempChanger.textContent = `${state.tempByDegree}`;
+  color = changeTempColor();
+  tempChanger.className = color;
+  //landscape
+  const landElement = document.querySelector('#landscape');
+  landscape = lanscapeScale();
+  landElement.textContent = landscape;
+};
+
+const selectSky = () => {
+  const skySelector = document.querySelector('#sky');
+  console.log(skySelector.value);
 };
 const tempDown = document.querySelector('#tempDown');
-tempDown.addEventListener('click', buttonDown);
+tempDown.addEventListener('click', clickButtonDown);
+
+//change temp range color
+const changeTempColor = () => {
+  let color = '';
+  if (state.tempByDegree < 49) {
+    color = 'teal';
+  } else if (state.tempByDegree < 60) {
+    color = 'green';
+  } else if (state.tempByDegree < 70) {
+    color = 'yellow';
+  } else if (state.tempByDegree < 80) {
+    color = 'orange';
+  } else {
+    color = 'red';
+  }
+  return color;
+};
+
+const lanscapeScale = () => {
+  let landscape = '';
+  if (state.tempByDegree < 49) {
+    landscape = '🌨❄️🥶🌬️❄️❄️⛄️⛄️⛄️🌬️🥶🌨';
+  } else if (state.tempByDegree < 60) {
+    landscape = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  } else if (state.tempByDegree < 70) {
+    landscape = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+  } else if (state.tempByDegree < 80) {
+    landscape = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+  } else {
+    landscape = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+  }
+  return landscape;
+};
+
+// Sky //
+const skySelector = document.querySelector('#sky');
+
+skySelector.addEventListener('change', selectSky);
+
+// City Name //
