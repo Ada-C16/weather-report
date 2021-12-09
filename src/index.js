0b1e6e6e1713f9c9bad8a0215834249e60552d10
roza ajema <mentoroza@yahoo.com>
2021-12-08 20:19:25 -0800

    adds drop-down selector for sky; and adds input element for city name

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+import "./index.css";
 
+let temperatureValue = document.getElementById('temp-value');
+temperatureValue.textContent = 71;
+// create event handler
+const changingTemps = () => {
+  temperatureValue += 1;
+  if (temperatureValue >= 80) {
+    temperatureValue.className = 'red';
+  } else if (temperatureValue < 80 && temperatureValue >= 70) {
+    temperatureValue.className = 'orange';
+  } else if (temperatureValue < 70 && temperatureValue >= 60) {
+    temperatureValue.className = 'yellow';
+  } else if (temperatureValue < 60 && temperatureValue >= 50) {
+    temperatureValue.className = 'green';
+  } else {
+    temperatureValue.className = 'teal';
+  }
+};
+//this is registering the event handler
+const registerChangingTemps = () => {
+  temp.addEventListener('click', changingTemps);
+};
 
-<script src="src/index.js"></script>
+document.addEventListener('DOMContentLoaded', registerChangingTemps);
