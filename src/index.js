1ad4e201fb6503bb33b747ba7636e56fc95218ba
brew doctor <>
2021-12-09 05:36:03 +0400

    added some more appearance-related things

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const setTemp = () => {
+  var tempLower = document.getElementById('lower temp');
+  var tempHigher = document.getElementById('higher temp');
+  if (tempLower) {
+    // temp=temp-1;
+    console.log(2);
+  } else {
+    // temp=temp+1
+    document.body.style.background = 'red';
+  }
+};
+// const tempColor = (temp) => {
+//   if (temp >= 35) {
+//     // temp color becomes red
+//     // background image becomes desert hot;
+//   }
+//   elif (temp<35 && temp>=25) {
+//     // temp color becomes orange
+//     // background image becomes warm spring;
+//   }
+//   elif (temp<25 && temp>=15) {
+// // temp color becomes green
+//     // background image becomes desert cool;
+//   }
+
+//   elif (temp<15 && temp>=5) {
+//     // temp color becomes blue
+//     // background image becomes cold;
+//   }
+
+//   else {
+//   // temp color becomes white
+//     // background image becomes ice cold;
+//   }
+// }
