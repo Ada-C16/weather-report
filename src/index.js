f99481a30122ecd24e11600ca54ec3cce90dc5d1
Rebecca Z <80494343+rzuick@users.noreply.github.com>
2021-12-09 13:06:49 -0600

    updated input funcationality to dynamically display text. created rand implemented reset button to reset text

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
+"use strict";
+
 const state = {
     temp: 70,
 };
 
 const changeCity = () => {
-    const textField = document.querySelector("#city");
+    const cityName= document.querySelector("#city");
     const textInput = document.querySelector("#typeCityName");
-    textField.innerHTML = textInput.value;
-    // textField.oninput = "changeCity()";
+    cityName.textContent = textInput.value;
 } 
+const resetCity = () => {;
+    const cityName = document.getElementById("city");
+    const textInput = document.querySelector("#typeCityName");
+    textInput.value = "San Antonio";
+    cityName.textContent = "San Antonio";
+}
 
-const increaseTemp = () => {
-    state.temp += 1;
+const changeTemp = (temp) => {
     const tempDisplay = document.querySelector("#tempDisplay");
     const landscape = document.querySelector("#landscape");
-    tempDisplay.textContent = `${state.temp}F`;
-    if (state.temp >= 80){
+    if (temp >= 80){
         tempDisplay.setAttribute("style", "background-color: red");
         landscape.src = "/assets/texasWeather.jpg";
-    } else if (state.temp >= 70){
+    } else if (temp >= 70){
         tempDisplay.setAttribute("style", "background-color: orange");
         landscape.src = "/assets/hotWeather.jpg";
-    } else if (state.temp >= 60){
+    } else if (temp >= 60){
         tempDisplay.setAttribute("style", "background-color: yellow");
         landscape.src = "/assets/springWeather.jpg";
-    } else if (state.temp >= 50){
+    } else if (temp >= 50){
         tempDisplay.setAttribute("style", "background-color: green; color: white");
         landscape.src = "/assets/fallWeather.jpg";
     } else {
@@ -32,31 +37,21 @@ const increaseTemp = () => {
     }
 }
 
+const increaseTemp = () => {
+    state.temp += 1;
+    tempDisplay.textContent = `${state.temp}F`;
+    changeTemp(state.temp);
+}
+
 const decreaseTemp = () => {
     state.temp -= 1;
-    const tempDisplay = document.querySelector("#tempDisplay");
-    const landscape = document.querySelector("#landscape");
     tempDisplay.textContent = `${state.temp}F`;
-    if (state.temp >= 80){
-        tempDisplay.setAttribute("style", "background-color: red");
-        landscape.src = "/assets/texasWeather.jpg";
-    } else if (state.temp >= 70){
-        tempDisplay.setAttribute("style", "background-color: orange");
-        landscape.src = "/assets/hotWeather.jpg";
-    } else if (state.temp >= 60){
-        tempDisplay.setAttribute("style", "background-color: yellow");
-        landscape.src = "/assets/springWeather.jpg";
-    } else if (state.temp >= 50){
-        tempDisplay.setAttribute("style", "background-color: green; color: white");
-        landscape.src = "/assets/fallWeather.jpg";
-    } else {
-        tempDisplay.setAttribute("style", "background-color: cyan; color: white");
-        landscape.src = "/assets/winterWeather.jpg";
-    }
+    changeTemp(state.temp);
 }
 
 const displayWeather = () => {
     const weatherDisplay = document.querySelector("#weatherSky");
+    const plainOption = document.querySelector("#plainSky");
     const sunnyOption = document.querySelector("#clearSky");
     const cloudyOption = document.querySelector("#cloudySky");
     const rainyOption = document.querySelector("#rainySky");
@@ -69,6 +64,8 @@ const displayWeather = () => {
         weatherDisplay.setAttribute("style", "background-image: url('/assets/Rainy_GIF.gif')");
     } else if (coldOption.selected) {
         weatherDisplay.setAttribute("style", "background-image: url('/assets/Cold_GIF.gif')");
+    } else {
+        weatherDisplay.setAttribute("style", "background-image: url('/assets/Plain_GIF.gif')");
     }
 }
 
@@ -76,11 +73,13 @@ const registerEventHandlers = () => {
     const tempIncreaseButton = document.querySelector("#tempIncrease");
     const tempDecreaseButton = document.querySelector("#tempDecrease");
     const weatherButton = document.querySelector("#changeSky");
-    const changesCity =  document.querySelector("#typeCityName").value;
+    const changesCity =  document.querySelector("#typeCityName");
+    const reset = document.getElementById("reset");
     tempIncreaseButton.addEventListener("click", increaseTemp);
     tempDecreaseButton.addEventListener("click", decreaseTemp);
     weatherButton.addEventListener("click", displayWeather);
-    changesCity.addEventListener("alert", changeCity)
+    changesCity.addEventListener("input", changeCity);
+    reset.addEventListener("click", resetCity);
 }
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
