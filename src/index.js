d385e33be9b57a04ba242751f0d81a43f9570f4f
Arianna Cabebe <aricabebe@gmail.com>
2021-12-09 14:25:55 -0800

    adds colorTempDisplay event handler

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ const state = {
 };
 
 //add helper functions to change temperature color
+const colorTempDisplay = () => {
+    if (state.temperature > 79) {
+        document.querySelector('#temperature-display').style.color = "#f07178";
+    } else if (state.temperature > 69 && state.temperature < 80) {        
+        document.querySelector('#temperature-display').style.color = "#f78c6c";
+    } else if (state.temperature > 59 && state.temperature < 70) {
+        document.querySelector('#temperature-display').style.color = "#ffcb6b";
+    } else if (state.temperature > 49 && state.temperature < 60) {
+        document.querySelector('#temperature-display').style.color = "#c3e88d";
+    } else if (state.temperature <= 49) {
+        document.querySelector('#temperature-display').style.color = "#89ddff";
+    }
+}
+
 
 //increase temp event handler
 const increaseTemp = () => {
@@ -23,10 +37,15 @@ const decreaseTemp = () => {
 }
 
 const registerEventHandlers = (event) => {
+    //increase temp
     const increaseTempButton = document.querySelector('#increase-temp');
     increaseTempButton.addEventListener("click", increaseTemp);
+    //decrease temp
     const decreaseTempButton = document.querySelector('#decrease-temp');
     decreaseTempButton.addEventListener("click", decreaseTemp);
+    //temperature color change
+    increaseTempButton.addEventListener("click", colorTempDisplay);
+    decreaseTempButton.addEventListener("click", colorTempDisplay);
 }
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
