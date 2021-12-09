a0d6ae39aea8469133a8ab40fabf487638b18175
Citlalli I Zarate <37793116+MiffyBruna@users.noreply.github.com>
2021-12-08 21:17:57 -0800

    Wave 2, method for temperature, color, icon change

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
 function up() {
-    var max='136'
-    document.getElementById("temperature").value = parseInt(document.getElementById("temperature").value) + 1;
-    if (document.getElementById("temperature").value >= parseInt(max)) {
-        document.getElementById("temperature").value = max;
-    }
+    var temperature = document.getElementById("temperature").value = parseInt(document.getElementById("temperature").value) + 1;
+    console.log(temperature);
+    currenttemperature(temperature);
 }
 function down() {
-    var min = '-89'
-    document.getElementById("temperature").value = parseInt(document.getElementById("temperature").value) - 1;
-    if (document.getElementById("temperature").value <= parseInt(min)) {
-        document.getElementById("temperature").value = min;
-    }
+    var temperature = document.getElementById("temperature").value = parseInt(document.getElementById("temperature").value) - 1;
+    console.log(temperature); 
+    currenttemperature(temperature);
 }
 
-window.onload = function() {
+const currenttemperature = (temperature) => {
+   
+    if(temperature >= 1 && temperature <= 49){
+        document.getElementById("temperature").style.color = "teal";
+        earth.textContent =  "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    } else if(temperature >= 50 && temperature <= 59 ){
+        document.getElementById("temperature").style.color = "green";
+        earth.textContent =  "🌲🍁🌲🍁🍂🌲🍁🌲🌲🍂🌲";
+    } else if(temperature >= 60 && temperature <= 69){
+        document.getElementById("temperature").style.color = "yellow";
+        earth.textContent =  "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+    } else if(temperature >= 70 && temperature <= 79){
+        document.getElementById("temperature").style.color = "orange";
+        earth.textContent =  "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+    } else if(temperature >= 80 && temperature <= 89){
+        document.getElementById("temperature").style.color = "red";
+        earth.textContent =  "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+    } else if (temperature >= 90 ){
+        document.getElementById("temperature").style.color = "red";
+        earth.textContent =  "🌵🏜🌵🔥🌵🦂🌵🏜🌵🔥🌵🦂";
+    } else if (Math.sign(temperature) === -0 ){
+        document.getElementById("temperature").style.color = "cyan";
+        earth.textContent =  "⛄️⛄️⛄️⛄️⛄️⛄️⛄️⛄️⛄️⛄️⛄️";
+    }
+
+}
+
+
+window.onload = function(){
     var up_btn = document.getElementById("weather-up");
     up_btn.onclick = up;
-    
+
     var down_btn = document.getElementById("weather-down");
     down_btn.onclick = down;
+
 }
+
