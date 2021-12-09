7d7354f78214b5def7fcf61687bf4dd7b14b4c6e
dynamicdev3000 <michellemoralesjs@gmail.com>
2021-12-09 17:34:07 -0500

    working temp & garden javascript, still need to work on name and garden placement

--- src/index.js
+++ src/index.js
@@ -1,27 +1,128 @@
-var data = 0;
+let currentTemp = 65;
 
-//printing default value of data that is 0 in h2 tag
-document.getElementById("counting").innerText = data;
+const updateTemperature = currentTemp => {
+    const tempContainer = document.getElementById("tempNum");
+    tempContainer.textContent = currentTemp;
+    changeTempColor(currentTemp);
+    changeLandscape(currentTemp);
+};
 
-//creation of increment function
-function increment() {
-    data = data + 1;
-    document.getElementById("counting").innerText = data;
-}
-//creation of decrement function
-function decrement() {
-    data = data - 1;
-    document.getElementById("counting").innerText = data;
-}
-//Weather Garden
-// if (
-//data <= 0
-//data >= 1 && data <=3
-//data >= 4 && data <=6
-//data >= 7 && data <=9
-//data >= 10 
+const increaseTemperature = () => {
+    currentTemp += 1;
+    updateTemperature(currentTemp);
+};
+
+const decreaseTemperature = () => {
+    currentTemp -= 1;
+    updateTemperature(currentTemp);
+};
+
+const changeTempColor = currentTemp => {
+    const tempContainer = document.getElementById("temp-content");
+    let color = "green";
+    if (currentTemp >= 80) {
+        color = "red";
+    } else if (currentTemp >= 70) {
+        color = "orange";
+    } else if (currentTemp >= 60) {
+        color = "yellow";
+    } else if (currentTemp >= 50) {
+        color = "green";
+    } else if (currentTemp < 50) {
+        color = "teal";
+    }
+    tempContainer.classList = color;
+};
+
+const changeLandscape = currentTemp => {
+    const landscapeContainer = document.getElementById("landscape");
+    let landscape = "";
+    if (currentTemp >= 80) {
+        landscape = "🌵🐍🦂🌵🌵🌵🐍🌵🏜🦂"
+    } else if (currentTemp >= 70) {
+        landscape = "🌸🌿🌼🌷🌻🌿☘️🌱🌻🌷"
+    } else if (currentTemp >= 60) {
+        landscape = "🌾🌾🍃🍃🪨🪨🪨🪨🍃"
+    } else if (currentTemp >= 50){ 
+        landscape = "🌲🌲⛄️🌲🍂🌲🌲🌲🍂🌲"
+    } else if (currentTemp < 50){
+        landscape = "⛄️❄️❄️⛄️❄️❄️⛄️⛄️❄️❄️"
+    }
+    landscapeContainer.textContent = landscape;
+};
+
+const updateSky = () => {
+    const skyDropdown = document.getElementById("sky-dropdown").value;
+    const skyForecast = document.getElementById("sky");
+    let sky = "";
+    if (skyDropdown === "Sunny") {
+        sky = "☀️☀️☀️☀️☀️☀️☀️☀️☀️"
+    } else if (skyDropdown === "Cloudy") {
+        sky = "🌤🌤🌤🌤🌤🌤🌤🌤🌤"
+    } else if (skyDropdown === "Rainy") {
+        sky = "🌧🌧🌧🌧🌧🌧🌧🌧🌧"
+    } else if (skyDropdown === "Snowy") {
+        sky = "❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️"
+    }
+    skyForecast.textContent = sky;
+};
+
+// const updateSky2 = () => {
+//     const skyDropdown = document.getElementById("sky-dropdown").value;
+//     const skyForecast = document.getElementsByTagName("header");
+//     // let header = " ";
+//     if (skyDropdown === "Sunny") {
+//         // sky = "☀️☀️☀️☀️☀️☀️☀️☀️☀️"
+//         header = document.getElementsByTagName("header").style.backgroundImage = "url('https://i.gifer.com/embedded/download/g0bK.gif')";
+//         // header = header.setAttribute('style', 'background:url(https://i.gifer.com/embedded/download/g0bK.gif) no-repeat !important');
+//         // style.backgroundImage = "url('https://i.gifer.com/embedded/download/g0bK.gif')"
+//     } else if (skyDropdown === "Cloudy") {
+//         sky = "🌤🌤🌤🌤🌤🌤🌤🌤🌤"
+//     } else if (skyDropdown === "Rainy") {
+//         sky = "🌧🌧🌧🌧🌧🌧🌧🌧🌧"
+//     } else if (skyDropdown === "Snowy") {
+//         sky = "❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️"
+//     }
+//     skyForecast.textContent = sky;
+// };
+
+const updateCityName = () => {
+    const inputCityName = document.getElementById("name").value;
+    const cityNameHeader = document.getElementById("name");
+    cityNameHeader.textContent = inputCityName;
+};
 
 
-// DOM listener
-clickBox.addEventListener("click", clickEffect);
+const resetCityName = () => {
+    const cityNameInput = document.getElementById("name");
+    cityNameInput.value = "Michelle";
+    updateCityName();
+};
+
+
+
+const registerEventHandlers = () => {
+    increaseTemperature();
+    const tempIncreaseBtn = document.getElementById("increase-temp");
+    tempIncreaseBtn.addEventListener("click", increaseTemperature);
+
+    decreaseTemperature();
+    const tempDecreaseBtn = document.getElementById("decrease-temp");
+    tempDecreaseBtn.addEventListener("click", decreaseTemperature);
+
+    updateSky();
+    const skyDropdown = document.getElementById("sky-dropdown");
+    skyDropdown.addEventListener("change", updateSky);
+
+
+    updateCityName();
+    const inputCityName = document.getElementById("city");
+    inputCityName.addEventListener("input", updateCityName);
+
+    resetCityName();
+    const cityNameResetBtn = document.getElementById("reset-button");
+    cityNameResetBtn.addEventListener("click", resetCityName);
+
+};
+
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
