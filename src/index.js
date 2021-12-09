fa99eff8b2f88612e79251c17077bea181a1da35
Vange Spracklin <houseofvange@gmail.com>
2021-12-09 09:21:37 -0800

    adds increaseTemperature js to hotter button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-const locations = {
-    
-}
+// 1. A click on the hotter button increases the counter by 1
+const state = {
+    degrees: 72,
+    clicked: false
+};
+
+const increaseTemp = (event) => {
+    const degreesContainer = document.getElementById("degrees");
+    state.degrees += 1;
+    degreesContainer.textContent = state.degrees;
+};
+
+// Register all events
+const registerEventHandlers = () => {
+    const hotButton = document.getElementById("hotterButton");
+    hotButton.addEventListener("click", increaseTemp);
+};
+
+// DOM listener
+document.addEventListener("DOMContentLoaded",registerEventHandlers);
