e6814728e0ed1062b65eea0ceeded305f546f130
ayaka <ayakaob@gmail.com>
2021-12-08 22:30:04 -0800

    add change view functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const state = {
   temp: 70,
+  sky: '',
 };
 
 const controlTemp = (e) => {
   changeTempNum(e);
   changeTempColor();
+  changeActivityView();
 };
 
 const changeTempNum = (e) => {
@@ -33,12 +35,55 @@ const changeTempColor = () => {
   } else {
     color = 'white';
   }
-
   tempText.style.color = color;
 };
 
+const changeActivityView = () => {
+  const activityEmoji = document.querySelector('.activity-emoji');
+  let emoji = '';
+  if (state.temp >= 87 && state.sky === 'sunny') {
+    emoji = 'hot and sunny';
+  } else if (state.temp >= 82) {
+    emoji = '🔥';
+  } else if (state.temp >= 68) {
+    emoji = '&#127957';
+  } else if (state.temp >= 60) {
+    emoji = '&#9918';
+  } else if (state.temp >= 50) {
+    emoji = '🔥';
+  } else {
+    emoji = '&#9924';
+  }
+  activityEmoji.innerHTML = emoji;
+};
+
 const controlSky = (e) => {
-  console.log(e.target.value);
+  state.sky = e.target.value;
+  changeActivityView();
+  changeSkyView();
+};
+
+const changeSkyView = () => {
+  const viewBg = document.querySelector('.view-background');
+  const skyEmoji = document.querySelector('.sky-emoji');
+  let emoji = '';
+  let color;
+
+  if (state.sky === 'sunny') {
+    emoji = '&#9728;';
+    color = 'skyblue';
+  } else if (state.sky === 'cloudy') {
+    emoji = '&#9729;';
+    color = 'gainsboro';
+  } else if (state.sky === 'rainy') {
+    emoji = '&#127783;';
+    color = 'lightgray';
+  } else if (state.sky === 'snowy') {
+    emoji = '&#10052;';
+    color = 'snow';
+  }
+  skyEmoji.innerHTML = emoji;
+  viewBg.style.backgroundColor = color;
 };
 
 const controlLocation = () => {
@@ -67,4 +112,9 @@ const registerHandler = () => {
   });
 };
 
-document.addEventListener('DOMContentLoaded', registerHandler);
+document.addEventListener('DOMContentLoaded', () => {
+  state.sky = document.getElementById('sky-condition').value;
+  changeSkyView();
+  changeActivityView();
+  registerHandler();
+});
