2d6bc36ba2ee3ade026899fd7e22887c6abdfa30
fan-gela <angelfan123@gmail.com>
2021-12-08 17:12:14 -0800

    working city input and header

--- src/index.js
+++ src/index.js
@@ -1,73 +1,137 @@
-// let temperature = 71;
-const temp = document.querySelector('#temp');
-let temp = 58;
+let count = 80;
 
-// function updateColorByTemp(newColor) {
-//     let temp = document.getElementById('temp');
-//     temp.style.color = newColor
+// const landscape = document.getElementById("landscape");
+// let landscape_text = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+// landscape.textContent = landscape
+
+// const sky = document.getElementById("sky");
+// let sky_text = "☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️";
+
+const increaseTemp = document.getElementById("increaseTemp");
+const decreaseTemp = document.getElementById("decreaseTemp");
+const counter = document.getElementById("count");
+counter.innerHTML = count;
+
+increaseTemp.addEventListener("click", function() {
+    counter.innerHTML = ++count;
+});
+
+decreaseTemp.addEventListener("click", function() {
+    counter.innerHTML = --count;
+});
+
+
+
+// const temp = document.getElementById('temp');
+// function increaseTemp() {
+//     temp.value = currentTemp
+//     currentTemp++;
 // }
 
-// constr temp = document.getElementById('temp');
+// function decreaseTemp() {
+//     temp.value = currentTemp
+//     currentTemp--;
+// }
 
-
-
-function increaseTemp() {
-    temp = temp + 1
+function changeColor(newColor) {
+    const elem = document.getElementById("count");
+    elem.style.color = newColor
+    // elem.style.backgroundColor = newColor
 }
 
-function decreaseTemp() {
-    temp = temp - 1
-}
-
-const updateColorAndLandscapeByTemp = (updatedTemp) => {
-    switch(updatedTemp){
-        case (updatedTemp >= 80) {
-            temp.style.color = 'red';
-            landscape.emoji = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
-            break;
+function updateColorAndLandscapeByTemp(count) {
+        if (count >= 80) {
+            // count.color = 'red';
+            changeColor('red');
+            landscape_text = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
         }
-        case (updatedTemp >= 79) {
-            temp.style.color = 'orange';
-            landscape.emoji = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
-            break;
-        }
-        case (updatedTemp >= 69) {
-            temp.style.color = 'yellow';
-            landscape.emoji = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
-            break;
-        
-        case (updatedTemp >= 59) {
-            temp.style.color = 'green';
-            landscape.emoji = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
-            break;
-        }
-        case (updatedTemp >= 49) {
-            temp.style.color = 'teal';
-            break;
+        else if (count >= 79) {
+            // count.style.color = 'orange';
+            changeColor('orange');
+            landscape_text = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
         }
     }
-}
 
-const sky = document.querySelector('#sky');
-const updateSkyEmoji = (updatedSky) => {
-    switch(updatedSky) {
-        case (updatedSky == "Sunny") {
-            sky.emoji = "☁️ ☁️ ☁️ ☀️ ☁️ ☁️";
-            break;
-        } case (updatedSky == "Cloudy") {
-            sky.emoji = "☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️";
-            break;
-        } case (updatedSky == "Rainy") {
-            sky.emoji = "🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧";
-            break;
-        } case (updatedSky == "Snowy") {
-            sky.emoji = "🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨";
-            break;
-        }
-    }
-}
+// function changeEmoji(emoji) {
+//     const elem = document.getElementById("landscape");
+//     elem.style.text
+// }
+// function updateColorAndLandscapeByTemp(updatedTemp) {
+//     switch(updatedTemp){
+//         case (updatedTemp >= 80):
+//             // count.color = 'red';
+//             changeColor('red');
+//             landscape.emoji = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+//             break;
+//         case (updatedTemp >= 79):
+//             // count.style.color = 'orange';
+//             changeColor('orange');
+//             landscape.emoji = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+//             break;
+//         case (updatedTemp >= 69):
+//             // count.style.color = 'yellow';
+//             changeColor('yellow');
+//             landscape.emoji = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+//             break;
+//         case (updatedTemp >= 59):
+//             // count.style.color = 'green';
+//             changeColor('green');
+//             landscape.emoji = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+//             break;
+//         case (updatedTemp >= 49):
+//             // count.color = 'teal';
+//             changeColor('teal');
+//             break;
+//     }
+// }
 
-const updateCity = (updatedCity) => {
-    const cityName = document.querySelector('#city');
-    cityName.textContent = updatedCity.target.value;
-}
+// const sky = document.querySelector('#sky');
+// const updateSkyEmoji = (updatedSky) => {
+//     switch(updatedSky) {
+//         case (updatedSky == "Sunny") {
+//             sky.emoji = "☁️ ☁️ ☁️ ☀️ ☁️ ☁️";
+                // skyBackground.style.backgroundColor = 'sunny';
+//             break;
+//         } case (updatedSky == "Cloudy") {
+//             sky.emoji = "☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️";
+//             break;
+//         } case (updatedSky == "Rainy") {
+//             sky.emoji = "🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧";
+//             break;
+//         } case (updatedSky == "Snowy") {
+//             sky.emoji = "🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨";
+//             break;
+//         }
+//     }
+// }
+
+// const updateCity = (updatedCity) => {
+//     const cityName = document.querySelector('#city');
+//     cityName.textContent = updatedCity.target.value;
+// }
+
+
+
+const updateCity = () => {
+    const cityInput = document.getElementById("cityInput").value;
+    const headerCity = document.getElementById("headerCityName");
+    headerCity.textContent = cityInput;
+};
+
+const resetCity = () => {
+    const cityInput = document.getElementById("cityInput");
+    cityInput.value = "Seattle";
+    updateCity();
+};
+
+updateCity();
+const cityNameInput = document.getElementById("cityInput");
+cityNameInput.addEventListener("input", updateCity);
+
+resetCity();
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", resetCity);  
+
+updateSky();
+const skySelect = document.getElementById("skyOptions");
+skySelect.addEventListener("change", updateSky);
