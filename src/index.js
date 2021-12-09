ccfce5b6c11411618ccbd32baaa157fae16863bc
Vange Spracklin <houseofvange@gmail.com>
2021-12-09 09:27:15 -0800

    adds decreaseTemp and niceDayTemp js to colder and nice day buttons

--- src/index.js
+++ src/index.js
@@ -10,10 +10,28 @@ const increaseTemp = (event) => {
     degreesContainer.textContent = state.degrees;
 };
 
+const decreaseTemp = (event) => {
+    const degreesContainer = document.getElementById("degrees");
+    state.degrees -= 1;
+    degreesContainer.textContent = state.degrees;
+};
+
+const niceDayTemp = (event) => {
+    const degreesContainer = document.getElementById("degrees");
+    state.degrees = 72;
+    degreesContainer.textContent = state.degrees;
+};
+
 // Register all events
 const registerEventHandlers = () => {
     const hotButton = document.getElementById("hotterButton");
     hotButton.addEventListener("click", increaseTemp);
+
+    const coldButton = document.getElementById("colderButton");
+    coldButton.addEventListener("click", decreaseTemp);
+
+    const niceDayButton = document.getElementById("niceDayButton");
+    niceDayButton.addEventListener("click", niceDayTemp);
 };
 
 // DOM listener
