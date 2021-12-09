50e674dcb3c665fca5c40ff5d9f28c8a34eabb02
Mariah Rosman <mrosman7@gmail.com>
2021-12-09 13:23:52 -0800

    adds sky functionality

--- src/index.js
+++ src/index.js
@@ -18,16 +18,53 @@ const updateTempState = () => {
   }
 };
 
+const tempColor = () => {
+  const tempGaugeElement = document.querySelector('#temp-gauge');
+
+  if (state.tempGauge >= 80) {
+    tempGaugeElement.className = "hot";
+  } else if (state.tempGauge >= 70) {
+    tempGaugeElement.className = "normal";
+  } else if (state.tempGauge >= 60) {
+    tempGaugeElement.className = "chilly";
+  } else if (state.tempGauge >= 50) {
+    tempGaugeElement.className = "cold";
+  } else if (state.tempGauge <= 49) {
+    tempGaugeElement.className = "freezing";
+  }
+}
+
 const tempUp = (event) => {
   console.log('in tempUp', event);
   state.tempGauge += 1;
   updateTempState();
+  tempColor();
 };
 
 const tempDown = (event) => {
   console.log('in tempDown', event);
   state.tempGauge -= 1;
   updateTempState();
+  tempColor();
+};
+
+const changeSky = (event) => {
+  const skyElement = document.querySelector("#sky-select");
+  const skyEmojis = document.querySelector("#sky-emojis");
+
+  if (skyElement.value === "cloudy") {
+    console.log(skyElement.value);
+    skyEmojis.textContent = `☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️`
+  } else if (skyElement.value === "rainy") {
+    console.log(skyElement.value);
+    skyEmojis.textContent = `🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧`
+  } else if (skyElement.value === "sunny") {
+    console.log(skyElement.value);
+    skyEmojis.textContent = `☁️ ☁️ ☁️ ☀️ ☁️ ☁️`
+  } else if (skyElement.value === "snowy") {
+    console.log(skyElement.value);
+    skyEmojis.textContent = `🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨`
+  };
 };
 
 const registerEventHandlers = (event) => {
@@ -36,6 +73,8 @@ const registerEventHandlers = (event) => {
   upButton.addEventListener('click', tempUp);
   const downButton = document.querySelector('#downButton');
   downButton.addEventListener('click', tempDown);
+  const skyElement = document.querySelector("#sky-select");
+  skyElement.addEventListener('change', changeSky);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
