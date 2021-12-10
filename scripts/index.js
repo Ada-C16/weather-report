0eab2c209d427da11f9ffd6fac44bd660d3f6d60
Sabrina Lauredan <slauredan@gmail.com>
2021-12-09 21:29:08 -0500

    reset working on city input field

--- scripts/index.js
+++ scripts/index.js
@@ -12,20 +12,18 @@ btn = document.getElementById('decrease_temperature');
 btn.onclick = () => changeTemp(-1);
 
 // change the city name everytime there is a change to the text input el
-// oninput or onchange to update city name
-// syntax = target.oninput = functionRef;
 
 let cityInput = document.querySelector('input');
 let newHeader = document.getElementById('header_city_id');
-
-// cityInput.oninput = changeCityNameByTextInput;
-
-// function changeCityNameByTextInput() {
-//   newHeader = cityInput;
-//   return newHeader;
-// }
 
 cityInput.oninput = function () {
   // this.value = this.value.toUpperCase();
   newHeader.innerHTML = cityInput.value;
 };
+
+// Resetting the city name header and city name input to default or blank
+
+function resetButton() {
+  let input = document.getElementById('cityNameInput');
+  input.value = 'New York';
+}
