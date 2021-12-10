ad66b4e4ddf70cf8b71aa453288ae28647d91488
Sabrina Lauredan <slauredan@gmail.com>
2021-12-09 20:21:31 -0500

    oninput working for city name

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-// funciton to change temp
-
 function changeTemp(x) {
   let el = document.getElementById('temperature_in_fahrenheit');
   let temp = el.innerText;
@@ -7,20 +5,27 @@ function changeTemp(x) {
   temp = String(temp);
   el.innerText = temp;
 }
-
-// function increaseTemp() {
-//   changeTemp(1);
-// }
-
-function decreaseTemp() {
-  changeTemp(1);
-}
-
+// change the temperature by on degree
 btn = document.getElementById('increase_temperature');
-// btn.onclick = increaseTemp;
-
 btn.onclick = () => changeTemp(1);
 btn = document.getElementById('decrease_temperature');
 btn.onclick = () => changeTemp(-1);
 
+// change the city name everytime there is a change to the text input el
 // oninput or onchange to update city name
+// syntax = target.oninput = functionRef;
+
+let cityInput = document.querySelector('input');
+let newHeader = document.getElementById('header_city_id');
+
+// cityInput.oninput = changeCityNameByTextInput;
+
+// function changeCityNameByTextInput() {
+//   newHeader = cityInput;
+//   return newHeader;
+// }
+
+cityInput.oninput = function () {
+  // this.value = this.value.toUpperCase();
+  newHeader.innerHTML = cityInput.value;
+};
