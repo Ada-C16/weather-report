70bccd182896fe1547591b14712342c226481fa9
Stephanie <stephanie.chenn1@gmail.com>
2021-12-09 23:33:58 -0500

    fixed class names

--- src/index.js
+++ src/index.js
@@ -1,126 +1,90 @@
 const state = {
-  tempCurrent: 60
+  tempCurrent: 60,
 };
 
-// Combine decreaseTemp and increaseTemp into one function during refactor
-// const decreaseTemp = () => {
-//   const currentTempEl = document.getElementById("currentTemp");
-
-//   state.tempCurrent -= 1
-//   currentTempEl.textContent = `${state.tempCurrent}°F`;
-
-//   changeLandscapeColor()
-// };
-
-// const increaseTemp =  () => {
-//   const currentTempEl = document.getElementById("currentTemp");
-
-//   state.tempCurrent += 1
-//   currentTempEl.textContent = `${state.tempCurrent}°F`;
-//   changeLandscapeColor()
-// };
-
+// Adjusting Temperature Depending on Which Button is Clicked
 const adjustTemp = (event) => {
-  const currentTempEl = document.querySelector("#currentTemp");
+  const currentTempEl = document.querySelector('#current-temp');
 
-  if (event.target.id === "decrease") {
+  if (event.target.id === 'decrease') {
     state.tempCurrent -= 1;
-  } 
-  else if (event.target.id === "increase") {
+  } else if (event.target.id === 'increase') {
     state.tempCurrent += 1;
   }
 
   currentTempEl.textContent = `${state.tempCurrent}°F`;
-  changeLandscapeColor()
-}
+  changeLandscapeColor();
+};
 
 // Adjusting Landscape & Background Color According to Temperature
 const changeLandscapeColor = () => {
-  const landscapeEmoji = document.querySelector("#landscape")
-  const tempSectionEl = document.querySelector(".tempDefault");
+  const landscapeEmoji = document.querySelector('#landscape');
+  const tempSectionEl = document.querySelector('.temp-default');
 
-  if(state.tempCurrent >=80) {
-    landscapeEmoji.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
-    tempSectionEl.id = "veryHotTemp";
-  }
-
-  else if (state.tempCurrent <=79 && state.tempCurrent >= 70) {
-    landscapeEmoji.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
-    tempSectionEl.id = "hotTemp";
-  }
-
-  else if(state.tempCurrent <=69 && state.tempCurrent >=60) {
-    landscapeEmoji.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
-    tempSectionEl.id = "middleTemp";
-  }
-
-  else if (state.tempCurrent <= 59 && state.tempCurrent >= 50) {
-    landscapeEmoji.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
-    tempSectionEl.id = "coldTemp";
-  }
-
-  else {
-    landscapeEmoji.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
-    tempSectionEl.id = "veryColdTemp";
+  if (state.tempCurrent >= 80) {
+    landscapeEmoji.textContent = '🌵🔥‼️🐌🐍🦂♨️🌵🔥🌵♨️🐍🦂🐍🏜';
+    tempSectionEl.id = 'veryHotTemp';
+  } else if (state.tempCurrent <= 79 && state.tempCurrent >= 70) {
+    landscapeEmoji.textContent = '🌸🌿🌼🌷🌿🌷🌻🌿🌱🌼🌱🌸🌻🌷';
+    tempSectionEl.id = 'hotTemp';
+  } else if (state.tempCurrent <= 69 && state.tempCurrent >= 60) {
+    landscapeEmoji.textContent = '🌾🪨🍁🍃🪨🍃🍂🍃💨🍁🍃🍂🏠💨🌲';
+    tempSectionEl.id = 'middleTemp';
+  } else if (state.tempCurrent <= 59 && state.tempCurrent >= 50) {
+    landscapeEmoji.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+    tempSectionEl.id = 'coldTemp';
+  } else {
+    landscapeEmoji.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+    tempSectionEl.id = 'veryColdTemp';
   }
 };
 
-// Adjusting Sky Levels According to Temperature
+// Adjusting Sky Levels
 const changeSky = () => {
-  const skySelectedEl = document.querySelector(".skyChoice");
-  const skyLandscapeEl = document.querySelector("#sky");
+  const skySelectedEl = document.querySelector('.sky-choice');
+  const skyLandscapeEl = document.querySelector('#sky');
 
-  if (skySelectedEl.value === "sunny") {
-    skyLandscapeEl.textContent = "☁️☀️☁️☀️☁️☀️☁️☀️☁️☀️☁️";
-  }
-
-  else if (skySelectedEl.value === "cloudy") {
-    skyLandscapeEl.textContent = "☁️☁️ 🌤 ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️";
-  }
-
-  else if (skySelectedEl.value === "rainy") {
-    skyLandscapeEl.textContent = "🌧🌈⛈🌧🌧💧⛈🌧🌧💧🌧";
-  }
-
-  else {
-    skyLandscapeEl.textContent = "🌨❄️🌨❄️🌨❄️🌨❄️🌨❄️❄️";
+  if (skySelectedEl.value === 'sunny') {
+    skyLandscapeEl.textContent = '☁️☀️☁️☀️☁️☀️☁️☀️☁️☀️☁️';
+  } else if (skySelectedEl.value === 'cloudy') {
+    skyLandscapeEl.textContent = '☁️☁️ 🌤 ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';
+  } else if (skySelectedEl.value === 'rainy') {
+    skyLandscapeEl.textContent = '🌧🌈⛈🌧🌧💧⛈🌧🌧💧🌧';
+    alert("Don't forget to bring an umbrella! It's raining outside!");
+  } else {
+    skyLandscapeEl.textContent = '🌨❄️🌨❄️🌨❄️🌨❄️🌨❄️❄️';
+    alert("Bundle up and don't forget your gloves! It's snowing outside!");
   }
 };
 
 // Changing city name
 const updateCity = (event) => {
-  const cityDisplay = document.getElementById("cityDisplay")
+  const cityDisplay = document.querySelector('#city-display');
   cityDisplay.textContent = event.target.value;
 };
 
 // Reset city button
 const resetCity = () => {
-  const cityInput = document.querySelector("#cityForm");
+  const cityInput = document.querySelector('#city-form');
   cityInput.reset();
-}
+};
 
 // Registering event handlers
 const registerHandlers = () => {
-  const decreaseButton = document.getElementById("decrease");
-  decreaseButton.addEventListener("click", adjustTemp);
+  const decreaseButton = document.querySelector('#decrease');
+  decreaseButton.addEventListener('click', adjustTemp);
 
-  const increaseButton = document.getElementById("increase");
-  increaseButton.addEventListener("click", adjustTemp);
+  const increaseButton = document.querySelector('#increase');
+  increaseButton.addEventListener('click', adjustTemp);
 
-  // const decreaseButton = document.getElementById("decrease");
-  // decreaseButton.addEventListener("click", decreaseTemp);
+  const selectSky = document.querySelector('.sky-choice');
+  selectSky.addEventListener('change', changeSky);
 
-  // const increaseButton = document.getElementById("increase");
-  // increaseButton.addEventListener("click", increaseTemp);
-
-  const selectSky = document.querySelector(".skyChoice");
-  selectSky.addEventListener("change", changeSky)
-
-  const cityInput = document.querySelector("input");
+  const cityInput = document.querySelector('input');
   cityInput.addEventListener('input', updateCity);
 
-  const resetCityButton = document.querySelector("#resetCity");
-  resetCityButton.addEventListener("click", resetCity)
+  const resetCityButton = document.querySelector('#reset-city');
+  resetCityButton.addEventListener('click', resetCity);
 };
 
-document.addEventListener("DOMContentLoaded", registerHandlers);
+document.addEventListener('DOMContentLoaded', registerHandlers);
