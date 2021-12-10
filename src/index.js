ad74ecc3d5f6c8caf8b2a3664ad351bd5c16e58c
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2021-12-10 09:47:01 -0800

    got the temperature button to change colors. added weather landscape function, building out the elif statements

--- src/index.js
+++ src/index.js
@@ -2,18 +2,46 @@ const state = {
     tempCount : 0
 };
 
+
+const changeTempColor = () => {
+    if (state.tempCount <= 49) {
+        document.getElementById("tempCounter").style.color = "teal";
+    } else if (state.tempCount <= 59) {
+        document.getElementById("tempCounter").style.color = "green";
+    } else if (state.tempCount <= 69) {
+        document.getElementById("tempCounter").style.color = "yellow";
+    } else if (state.tempCount <= 79) {
+        document.getElementById("tempCounter").style.color = "orange";
+    } else if (state.tempCount >= 80) {
+        document.getElementById("tempCounter").style.color = "red";
+    } 
+}
+
 const decreaseTemp = () => {
     state.tempCount -= 1;
     const tempCountContainer = document.querySelector("#tempCounter");
     tempCountContainer.textContent = `${state.tempCount}`;
+    changeTempColor();
+    changeLandscape();
 }
-
 
 const increaseTemp = () => {
     state.tempCount += 1;
     const tempCountContainer = document.querySelector("#tempCounter");
     tempCountContainer.textContent = `${state.tempCount}`;
+    changeTempColor();
+    changeLandscape();
 }
+
+const changeLandscape = () => {
+    if (state.tempCount === 0) {
+        document.getElementById("landscape").textContent = "🍓";
+    } else if (state.tempCount > 0) {
+        document.getElementById("landscape").textContent = "hi";
+    }
+}
+
+
 
 const registerEventHandlers = () => {
     const decButton = document.querySelector("#decreaseButton");
