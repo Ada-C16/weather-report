c80e3b93ef15fa9ac1e0ffacb328ace72fd9ac54
Edmine <edmine.de@gmail.com>
2021-12-12 02:48:50 -0500

    saved all thee sky elements

--- src/index.js
+++ src/index.js
@@ -1,68 +1,82 @@
 
-const mainSection = document.querySelector("#weather-title");
+// const mainSection = document.querySelector("#weather-title");
 
 
-const title = document.createElement("h1");
+// const title = document.createElement("h1");
 
-title.textContent = " Min's Weather Report";
+// title.textContent = " Min's Weather Report";
 
-mainSection.appendChild(title);
-console.log(weatherTitle);
-console.log(title);
+// mainSection.appendChild(title);
+// console.log(weatherTitle);
+// console.log(title);
 
 const state = {
-    tempcount : 0 
+    tempchange: 50 
+
+}
+//selecting the variable increase the value of temperature(tempvalue) so we can use 
+const clickButtonUp = () => {
+    state.tempchange += 1;
+    const increseButtonElement = document.getElementById('tempvalue');
+    increseButtonElement.textContent = `${state.tempchange}`;
+    console.log("clickly click click");
+
 }
 
-//Create event handler for clicking on temperature button 
-// const addButton = (event)=>{
-//     const increaseButton = document.createElement("span");
-//     const decreaseButton = document.createElement("span");
-//     const tempButtonscontainer = document.querySelector("#tempButtons");
-//     increaseButton.textContent = "🥵";
-//     decreaseButton.textContent = "🥶";
-//     tempButtonscontainer.appendChild(increaseButton,decreaseButton.textContent)
 
-//     state.tempcount+=1
-//     state.tempcount-=1
-
-// }
+// const updateSky = () => {
+//     const inputSky = document.getElementById("skySelect").value;
+//     const skyContainer = document.getElementById("sky");
+//     let sky = "";
+//     let skyColor = "";
+//     if (inputSky === "Cloudy") {
+//         sky = url(/upintheclouds.webp);
+//         skyColor = "cloudy";
+//     } else if (inputSky === "Sunny") {
+//         sky = "☁️     ☁️   ☁️ ☀️ ☁️  ☁️";
+//         skyColor = "sunny";
+//     } else if (inputSky === "Rainy") {
+//         sky = "🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧";
+//         skyColor = "rainy";
+//     } else if (inputSky === "Snowy") {
+//         sky = "🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨";
+//         skyColor = "snowy";
+//     }
+//     skyContainer.textContent = sky;
+//     const gardenContent = document.getElementById("gardenContent");
+//     gardenContent.classList = `garden__content ${skyColor}`;
+// };
 const updateTemp = tempValue => {
-    const tempValueContainer = document.getElementById("tempValue");
+    const tempValueContainer = document.getElementById("tempvalue");
     tempValueContainer.textContent = tempValue;
     updateTempStyles(tempValue);
     updateGarden(tempValue);
+}
 
-const increaseTemp = () => {
-    tempValue += 1;
-    updateTemp(tempValue);
-};
-
-const decreaseTemp = () => {
-    tempValue -= 1;
-    updateTemp(tempValue);
-};
-const registerEventHandlers = () => {
-    updateTemp(tempvalue);
-
-    const increaseTempControl = document.getElementById("increaseTempControl");
-    increaseTempControl.addEventListener("click", increaseTemp);
-    
-    const decreaseTempControl = document.getElementById("decreaseTempControl");
-    decreaseTempControl.addEventListener("click", decreaseTemp);
+const updateTempStyles = (currentTemp) => {
+    const tempValueContainer = document.getElementById("tempvalue");
+    let color = "teal";
+    if ( currentTemp >= 80) {
+        color = "red";
+    } else if (currentTemp >= 70) {
+        color = "orange";
+    } else if (currentTemp >= 60) {
+        color = "yellow";
+    } else if (currentTemp >= 50) {
+        color = "green";
+    }
+    tempValueContainer.classList = color;
+}
 
 
-// const registerEvents = () => {
-//     updateTemp("included");
 
-//     const clickMeBtn = document.addEventListener("DOMContentloaded", registerEvents)
-// }; 
+    //register the button, telling the website to listen for a click 
+const registerEventHandlers = () =>{
+    const increseButtonElement = document.getElementById('increseButton');
+    increseButtonElement.addEventListener("click", clickButtonUp)
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
+}
 
-// const registerEventHandlers = () => {
-//     updateTemp(tempValue);
 
-//     const increaseTempControl = document.getElementById("increaseTempControl");
-//     increaseTempControl.addEventListener("click", increaseTemp);
-    
-//     const decreaseTempControl = document.getElementById("decreaseTempControl");
-//     decreaseTempControl.addEventListener("click", decreaseTemp);
+
+
