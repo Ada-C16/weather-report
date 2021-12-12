cfdc8a5ca1d6a2b521f3b9ffbeea57d32c7d6a3d
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2021-12-12 11:19:15 -0800

    trying to get the drop down menu to work

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const state = {
     tempCount : 0
 };
 
-
 const changeTempColor = () => {
     if (state.tempCount <= 49) {
         document.getElementById("tempCounter").style.color = "teal";
@@ -15,6 +14,23 @@ const changeTempColor = () => {
     } else if (state.tempCount >= 80) {
         document.getElementById("tempCounter").style.color = "red";
     } 
+}
+
+const changeLandscape = () => {
+    if (state.tempCount <= 59) {
+        document.getElementById("landscape").textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    } else if (state.tempCount <= 69) {
+        document.getElementById("landscape").textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+    } else if (state.tempCount <= 79) {
+        document.getElementById("landscape").textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+    } else if (state.tempCount >= 80) {
+        document.getElementById("landscape").textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+    }
+}
+
+const changeSky = () => {
+    var value = selectObject.value;  
+    console.log(value);
 }
 
 const decreaseTemp = () => {
@@ -33,22 +49,15 @@ const increaseTemp = () => {
     changeLandscape();
 }
 
-const changeLandscape = () => {
-    if (state.tempCount === 0) {
-        document.getElementById("landscape").textContent = "🍓";
-    } else if (state.tempCount > 0) {
-        document.getElementById("landscape").textContent = "hi";
-    }
-}
-
-
-
 const registerEventHandlers = () => {
     const decButton = document.querySelector("#decreaseButton");
     decButton.addEventListener("click", decreaseTemp);
     
     const incButton = document.querySelector("#increaseButton");
     incButton.addEventListener("click", increaseTemp);
+
+    const skyMenu = document.querySelector("#sky");
+    skyMenu.addEventListener("onChange", changeSky)
 }
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
