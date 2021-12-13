2495500bce6e664f697da1df1c09e4de26175026
Diana <diana.lozanocalderon@gmail.com>
2021-12-13 05:29:32 -0800

    Validated HTML and CSS

--- src/index.js
+++ src/index.js
@@ -1,48 +1,89 @@
-src= "/styles/index.css";
+// src= "/styles/index.css";
 
 /*
 1. Functions: 
-- background changes
-- temperature display
-- temp up element (clickable)
-- temp down element (clickable)
-- landscape display
-- sky type. dropdown element <select>
+x temperature display
+x temp up element (clickable)
+x temp down element (clickable)
+x landscape display
+x sky type. dropdown element <select>
 - sky display 
 - city name display
 - rename city element with <input type = "text">
 - reset city name element(clickable)*/
 
+//DOM: How we manipulate websites
+//Nodes: Tags or elements inside the DOM
 
+//Defining the base temperature by creating the state object
+const state = {
+    temperature: 40
+};
+console.log(state)
 
-const temperature = (itsCold, itsHot, clothes) => {
+//Changing the closet
+const temperature = () => {
+    const closet = document.querySelector('#emojicloset')
     if (state.temperature <= 30) {
-        itsCold.className = 'winter';
-        clothes.textContent = 'emoji'; //winter clothes;
-        const eventBackground = document.getElementById('event-background')
-        eventBackground.changeClass
+        closet.textContent = '🧣🧤'; //winter clothes;
     } else if (31 <= state.temperature && state.temperature <= 55) {
-        itsCold.className = 'fall';
-        clothes.textContent = 'fall emoji'; //fall clothes
+        closet.textContent = '🧥'; //fall clothes
     } else if (56 <= state.temperature && state.temperature <= 70) {
-        itsHot.className = 'spring';
-        clothes.textContent = 'spring emoji' //spring clothes
+        closet.textContent = '👗' //spring clothes
     } else {
-        itsHot.className = 'summer';
-        clothes.textContent = 'summer emoji' //summer clothes
-    }
-    } 
+        closet.textContent = '🕶👙' //summer clothes
+    }}
 
-const registerEventhandler = () => {
-    const changeTemperatureHot = document.getElementById('itsHot-button');
-    changeTemperatureHot.addEventListener('click', increaseTemperature)
-
-    const changeTemperatureCold = document.getElementById('itsCold-button');
-    changeTemperatureCold.addEventListener('change', decreaseTemperature)
+//Changing Temperature
+const upTemperature = () => {
+    const tempUp = document.getElementById('base-temp');
+    //Makes temp count go up
+    state.temperature += 1;
+    //Making the text content o tempUp = the current temperature
+    tempUp.textContent = `${state.temperature}`;
+    //querySelector: Returns the first element
+    const tempUptemp = document.querySelector('#temperature')
+    const clothes = document.getElementById('clothes')
+    temperature()
 }
+const downTemperature = () => {
+    const tempDown = document.getElementById('base-temp');
+    state.temperature -= 1;
+    //Returns every element in the Node
+    tempDown.textContent = `${state.temperature}`;
+    //This line is selecting the temperature id
+    const tempDowntemp = document.querySelector('#temperature')
+    const clothes = document.getElementById('clothes')
+    temperature()
+}
+////Defining the base sky by creating the state object
+const skyState = {
+    sky: 1
+};
+console.log(skyState)
 
-document.addEventListener('DOMContentLoaded', registerEventhandler)
+const sky = () => {
+    const skyMenu = document.querySelector('#emojisky')
+    if (skyState.sky === 'snowy') {
+        skyMenu.textContent = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨"'; //winter clothes;
+    } else if (skyState.sky == 'Sunny') {
+        skyMenu.textContent = 'sun'; //fall clothes
+    } else if (skyState.sky  = 'cloudy') {
+        skyMenu.textContent = 'cloud' //spring clothes
+    } else {
+        skyMenu.textContent = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧'; 
+    }}
 
+//Register Events
+const registerEventhandlers = () => {
+    const TemperatureHot = document.getElementById('itsHot');
+    TemperatureHot.addEventListener('click', upTemperature)
 
+    const TemperatureCold = document.getElementById('itsCold');
+    TemperatureCold.addEventListener('click', downTemperature)
 
+    const skyPresentation = document.getElementById('sky');
+    skyPresentation.addEventListener('onclick', sky)
+};
+document.addEventListener("DOMContentLoaded", registerEventhandlers);
 
