93efca49eb80473802e8fb35adb0834b56116ce9
Vange Spracklin <houseofvange@gmail.com>
2021-12-13 10:19:26 -0800

    adds incomplete reset button.

--- src/index.js
+++ src/index.js
@@ -74,7 +74,7 @@ const changeLocationTitle = (event) => {
     const result = document.getElementById("locationTitle");
     result.textContent = `${event.target.value}`;
     const inputField = document.getElementById("nameThisPlaceText")
-    inputField.value = ""
+    inputField.value = "";
 };
 
 // Name this place input is filled out by user
@@ -89,13 +89,29 @@ const changeCloudAnimation = (event) => {
     const clouds = document.getElementById("cloud");
     const weatherpicked = `${event.target.value}`;
     if (weatherpicked == 'sunny'){
-        clouds.innerHTML = `<img src="assets/sunny_cloud.gif" alt="white cloud with smiling face softly bobs a bit" id="sunny_cloud"></img>`
-    } else if (weatherpicked == 'cloudy'){
-        clouds.innerHTML = `<img src="assets/stormy_cloud.gif" alt="dark grey and blue cloud with devious facial expression rains down water and lightning" id="stormy_cloud">`
+        clouds.innerHTML = `<img src="assets/sunny_cloud.gif" alt="white cloud with smiling face softly bobs a bit" id="sunny_cloud"></img>`;
+    } else if (weatherpicked == 'stormy'){
+        clouds.innerHTML = `<img src="assets/stormy_cloud.gif" alt="dark grey and blue cloud with devious facial expression rains down water and lightning" id="stormy_cloud">`;
     } else if (weatherpicked == 'windy'){
-        clouds.innerHTML = `<img src="assets/windy_cloud.gif" alt="white cloud with smiling face softly bobs a bit" id="windy_cloud">`
-    } else if (weatherpicked == 'clearsky'){
-        clouds.textContent = ""
+        clouds.innerHTML = `<img src="assets/windy_cloud.gif" alt="white cloud with smiling face softly bobs a bit" id="windy_cloud">`;
+    } else if (weatherpicked == 'clear sky'){
+        clouds.textContent = "";
+    }
+}
+
+// Change location landmark landmark animations
+const changeLandmarkAnimation = (event) => {
+    const landmarks = document.getElementById("landmark");
+    const locationPicked = `${event.target.value}`;
+    console.log(`something ${event.target.value}`);
+    if (locationPicked == 'Muscle Beach🏋️'){
+        landmarks.innerHTML = `<img src="./assets/muscle_beach.gif" id="muscle_beach">`;
+    } else if (locationPicked == 'Disneyland🎢'){
+        landmarks.innerHTML = `<img src="./assets/mickey.gif" id="mickey">`;
+    } else if (locationPicked == 'LAX🛫'){
+        landmarks.innerHTML = `<img src="./assets/lax_plane.png" id="lax_plane">`;
+    // } else if (locationPicked == ''){
+    //     landmarks.textContent = "";
     }
 }
 
@@ -106,6 +122,25 @@ const replaceWithLiveSantaMonica = (event) => {
     
     // hmm remember to change the text of the viewLivebutton
 };
+
+// reset everything
+const reset = (event) => {
+    const landmarks = document.getElementById("landmark");
+    landmarks.innerHTML = `<img src="assets/left_palm.gif" id="left_palm">
+    <img src="assets/AngelyneNFT.gif" id="angelyne">
+    <img src="assets/right_palms.gif" id="left_palm">`;
+    const locationTitle = document.getElementById("locationTitle");
+    locationTitle.textContent = `SoCal`;
+    const clouds = document.getElementById("cloud");
+    clouds.textContent = "";
+    const degreesContainer = document.getElementById("degrees");
+    state.degrees = 72
+    degreesContainer.textContent = state.degrees;
+    const inputField = document.getElementById("nameThisPlaceText");
+    inputField.value = "";
+    const locationSelector = document.getElementById('locations');
+    locationSelector.options = "SoCal";
+}
 
 // Register all events
 const registerEventHandlers = () => {
@@ -134,6 +169,7 @@ const registerEventHandlers = () => {
     // Change the location title at the top of the page after user selects a location
     const selectLocation = document.querySelector('.locations');
     selectLocation.addEventListener('change', changeLocationTitle);
+    selectLocation.addEventListener('change', changeLandmarkAnimation)
 
     // Change the location title at the top of the page after user enters custom location name.
     const enterLocationName = document.getElementById("nameThisPlaceText");
@@ -146,6 +182,10 @@ const registerEventHandlers = () => {
     // add live video
     const viewLiveSantaMonicaButton = document.getElementById("viewLiveButton");
     viewLiveSantaMonicaButton.addEventListener("click", replaceWithLiveSantaMonica);
+
+    // reset everything
+    const resetButton = document.getElementById("resetButton");
+    resetButton.addEventListener('click', reset)
 };
 
 // DOM listener
