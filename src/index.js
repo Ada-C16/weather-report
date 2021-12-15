5690a5ef690b124e58745d8efed98d7591ca3377
Sophia Le <sophiale@sophias-mbp.lan>
2021-12-15 14:43:06 -0800

    Adds landscapeHandler

--- src/index.js
+++ src/index.js
@@ -8,14 +8,25 @@ const registerEventHandlers = () => {
     document.getElementById("decrement-btn").addEventListener('click', tempHandler(-1));
 };
 
-
-
 const tempHandler = (step) => {
     const tempInput = document.getElementById("tempInput");
     const tempInt = parseInt(tempInput.textContent) + step;
     console.log(tempInt);
     tempInput.textContent = String(tempInt);
     state.temp = tempInt;
-    //update garden
+    landscapeHandler()
+};
+
+const landscapeHandler = () => {
+    const landscape = document.getElementById("landscapeOutput");
+    if (state.temp >= 80) {
+        landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+    } else if (state.temp >= 70 && state.temp < 80) {
+        landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+    } else if (state.temp >= 60 && state.temp < 70) {
+        landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+    } else {
+        landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    }
 };
 
