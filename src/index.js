5d6ba182e759cd174c2558be78ec7f30547f1bd9
Mary <marytian.mt@gmail.com>
2022-01-27 13:52:41 -0700

    Fixed typo calling tempFarenheit. Added dynamic containers to sky and temp.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+//use this for anything that's dynamic, ie something that changes due to an action
 
 const state = {
     tempIncreaseCount: 70,
@@ -8,15 +8,28 @@ const state = {
 const updateTempIncrease = () => {
     const increaseTempContainer= document.getElementById("tempFarenheit");
     state.tempIncreaseCount +=1;
-    increaseTempContainer.textContent = state.tempIncreaseCount
+    increaseTempContainer.textContent = state.tempIncreaseCount;
+    updateTemp(state.tempIncreaseCount);
 }
 
 const updateTempDecrease = () => {
     const decreaseTempContainer= document.getElementById("tempFarenheit");
     state.tempIncreaseCount -=1;
-    decreaseTempContainer.textContent = state.tempIncreaseCount
+    decreaseTempContainer.textContent = state.tempIncreaseCount;
+    updateTemp(state.tempIncreaseCount);
 }
 
+const updateCityName = () => {
+    const inputName = document.getElementById("cityNameInput").value;
+    const headerCityName = document.getElementById("headerCityName");
+    headerCityName.textContent = inputName;
+};
+
+const resetCityName = () => {
+    const cityNameInput = document.getElementById("cityNameInput");
+    cityNameInput.value = "Denver";
+    updateCityName();
+};
 
 const updateSky = () => {
     const inputSky = document.getElementById("skySelect").value;
@@ -41,6 +54,56 @@ const updateSky = () => {
     // gardenContent.classList = `garden__content ${skyColor}`;
 };
 
+const updateScenery = (currentTemp) => {
+    const sceneryContainer = document.getElementById("scenery");
+    let scenery = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    if ( currentTemp >= 80) {
+        scenery = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+    } else if (currentTemp >= 70) {
+        scenery = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+    } else if (currentTemp >= 60) {
+        scenery = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+    }
+    sceneryContainer.textContent = scenery;
+}
+
+const updateTempStyles = (currentTemp) => {
+    const tempValueContainer = document.getElementById("temperature_background");
+    let color = "teal";
+    if (currentTemp >= 80) {
+        color = "red";
+    } else if (currentTemp >= 70) {
+        color = "orange";
+    } else if (currentTemp >= 60) {
+        color = "yellow";
+    } else if (currentTemp >= 50) {
+        color = "green";
+    }
+    tempValueContainer.classList = color;
+}
+
+const updateSkyBackground = (currentTemp) => {
+    const tempValueContainer = document.getElementById("sky_background");
+    let color = "teal";
+    if (currentTemp >= 80) {
+        color = "red";
+    } else if (currentTemp >= 70) {
+        color = "orange";
+    } else if (currentTemp >= 60) {
+        color = "yellow";
+    } else if (currentTemp >= 50) {
+        color = "green";
+    }
+    tempValueContainer.classList = color;
+}
+
+const updateTemp = (tempFarenheit) => {
+    const tempFarenheitContainer = document.getElementById("tempFarenheit");
+    tempFarenheitContainer.textContent = tempFarenheit;
+    updateTempStyles(tempFarenheit);
+    updateSkyBackground(tempFarenheit);
+    updateScenery(tempFarenheit);
+}
 // const increaseTemp = () => {
 //     tempValue += 1;
 //     updateTemp(tempValue);
@@ -62,12 +125,12 @@ const registerEventHandlers = () => {
     const decreaseTempControl = document.getElementById("decreaseTemp");
     decreaseTempControl.addEventListener("click", updateTempDecrease);
 
-    // updateCityName();
-    // const cityNameInput = document.getElementById("cityNameInput");
-    // cityNameInput.addEventListener("input", updateCityName);
+    updateCityName();
+    const cityNameInput = document.getElementById("cityNameInput");
+    cityNameInput.addEventListener("input", updateCityName);
 
-    // const cityNameResetBtn = document.getElementById("cityNameReset");
-    // cityNameResetBtn.addEventListener("click", resetCityName);
+    const cityNameResetBtn = document.getElementById("cityNameReset");
+    cityNameResetBtn.addEventListener("click", resetCityName);
 
     updateSky();
     const skySelect = document.getElementById("skySelect");
